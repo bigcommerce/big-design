16f067cce0f49b7f649523c129d26970bafcd5b0
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-03-11 09:20:42 -0500

    chore(all): upgrade dependencies

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,4 +1,4 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
 
 const req = require.context('../stories', true, /.stories.tsx$/);
 
@@ -6,5 +6,17 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
+addParameters({
+  backgrounds: [
+    {
+      default: true,
+      name: 'Grey',
+      value: '#f6f7f9',
+    },
+    {
+      name: 'White',
+      value: '#ffffff',
+    },
+  ],
+});
 configure(loadStories, module);
-
