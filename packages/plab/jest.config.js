b519d3c169a97fcd805e811870169a73997f177d
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-03-18 09:20:58 -0500

    fix(plab): tests now run with lerna setup

--- packages/plab/jest.config.js
+++ packages/plab/jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  transform: {
-    '.[jt]sx?$': '<rootDir>/node_modules/babel-jest',
-  },
   testRegex: '(<rootDir>/(tests|src)/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
