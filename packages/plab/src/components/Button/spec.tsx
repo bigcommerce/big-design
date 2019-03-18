3bcc444e2f9522fa3d292ece72172ee979bbc2cb
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-03-18 14:15:23 -0500

    test(plab): basic button render tests

--- packages/plab/src/components/Button/spec.tsx
+++ packages/plab/src/components/Button/spec.tsx
@@ -4,8 +4,116 @@ import renderer from 'react-test-renderer';
 
 import { Button } from './Button';
 
-test('sample test', () => {
-  const tree = renderer.create(<Button />);
+test('render default button', () => {
+  const tree = renderer.create(<Button>Button</Button>).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render disabled button', () => {
+  const tree = renderer.create(<Button disabled>Button</Button>).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render destructive button', () => {
+  const tree = renderer.create(<Button actionType="destructive">Button</Button>).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render destructive disabled button', () => {
+  const tree = renderer
+    .create(
+      <Button actionType="destructive" disabled>
+        Button
+      </Button>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render secondary button', () => {
+  const tree = renderer.create(<Button variant="secondary">Button</Button>).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render secondary disabled button', () => {
+  const tree = renderer
+    .create(
+      <Button variant="secondary" disabled>
+        Button
+      </Button>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render secondary destructive button', () => {
+  const tree = renderer
+    .create(
+      <Button variant="secondary" actionType="destructive">
+        Button
+      </Button>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render secondary destructive disabled button', () => {
+  const tree = renderer
+    .create(
+      <Button variant="secondary" actionType="destructive" disabled>
+        Button
+      </Button>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render subtle button', () => {
+  const tree = renderer.create(<Button variant="subtle">Button</Button>).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render subtle disabled button', () => {
+  const tree = renderer
+    .create(
+      <Button variant="subtle" disabled>
+        Button
+      </Button>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render subtle destructive button', () => {
+  const tree = renderer
+    .create(
+      <Button variant="subtle" actionType="destructive">
+        Button
+      </Button>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('render subtle destructive disabled button', () => {
+  const tree = renderer
+    .create(
+      <Button variant="subtle" actionType="destructive" disabled>
+        Button
+      </Button>,
+    )
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
