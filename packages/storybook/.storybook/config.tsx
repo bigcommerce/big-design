afda3a70c17f05e8d87cf896d1cce99a7ef56829
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-03-19 10:36:28 -0500

    chore(storybook): change directory structure

---
+++ packages/storybook/.storybook/config.tsx
@@ -0,0 +1,48 @@
+import { GlobalStyle } from '@bigcommerce/plab';
+import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator, addParameters, configure } from '@storybook/react';
+import React from 'react';
+
+const req = require.context('../stories', true, /\.story\.tsx$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+addParameters({
+  viewport: {},
+  backgrounds: [
+    {
+      default: true,
+      name: 'Grey',
+      value: '#f6f7f9',
+    },
+    {
+      name: 'White',
+      value: '#ffffff',
+    },
+  ],
+});
+
+addDecorator(withKnobs);
+
+addParameters({
+  a11y: {
+    options: {
+      rules: {
+        'form-field-multiple-labels': {
+          enabled: false,
+        },
+      },
+    },
+  },
+});
+
+addDecorator(storyFn => (
+  <React.Fragment>
+    <GlobalStyle />
+    {storyFn()}
+  </React.Fragment>
+));
+
+configure(loadStories, module);
