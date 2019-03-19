ad3abef79b3750e3539f9110fc7fc8fb21b1fd3f
Roberto Alvarez <robertoatc02@gmail.com>
2019-03-19 15:07:15 -0500

    feat(plab): add Link component

---
+++ packages/storybook/src/Link.story.tsx
@@ -0,0 +1,15 @@
+import { H3, Link, Panel } from '@bigcommerce/plab';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+storiesOf('Link', module).add('Overview', () => (
+  <div style={{ padding: '2rem' }}>
+    <Panel>
+      <H3>Links that point to other places</H3>
+      Links are not underlined and have the correct color.&nbsp;
+      <Link href="https://www.bigcommerce.com" target="_blank">
+        Learn more.
+      </Link>
+    </Panel>
+  </div>
+));
