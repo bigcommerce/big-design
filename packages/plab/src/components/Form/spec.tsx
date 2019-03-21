5aa43ad82cf1b562fd031b083faad3fabf585481
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-03-21 14:44:37 -0500

    test(plab): form + input tests

--- packages/plab/src/components/Form/spec.tsx
+++ packages/plab/src/components/Form/spec.tsx
@@ -2,6 +2,8 @@ import 'jest-styled-components';
 import React from 'react';
 import { fireEvent, render } from 'react-testing-library';
 
+import { Input } from '../Input';
+
 import { Form } from './index';
 
 test('forwards ref', () => {
@@ -21,4 +23,33 @@ test('calls onSubmit', () => {
   fireEvent.submit(form);
 
   expect(onSubmit).toHaveBeenCalled();
+});
+
+test('simple form render', () => {
+  const { container } = render(
+    <Form>
+      <Form.Fieldset
+        legend="Primary contact"
+        description="Minim velit quis aute adipisicing adipisicing do do exercitation cupidatat enim ex voluptate consequat labore."
+      >
+        <Form.Row>
+          <Input
+            label="First Name"
+            description="This is an example description for First Name"
+            placeholder="Placeholder text"
+          />
+        </Form.Row>
+
+        <Form.Row>
+          <Input
+            label="Middle Name"
+            description="This is an example description for Last Name. Featuring a Left Icon."
+            placeholder="Placeholder text"
+          />
+        </Form.Row>
+      </Form.Fieldset>
+    </Form>,
+  );
+
+  expect(container.firstChild).toMatchSnapshot();
 });
