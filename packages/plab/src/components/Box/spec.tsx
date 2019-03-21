d5939c0cd9d95c4d15d6365618007fe49e5564f7
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-03-21 12:24:27 -0500

    test(plab): box component tests

--- packages/plab/src/components/Box/spec.tsx
+++ packages/plab/src/components/Box/spec.tsx
@@ -4,7 +4,7 @@ import { render } from 'react-testing-library';
 
 import { Box } from './index';
 
-test('box should have margin props', () => {
+test('has margin props', () => {
   const { container, rerender } = render(<Box />);
 
   expect(container.firstChild).not.toHaveStyle('margin: 1rem');
@@ -12,4 +12,46 @@ test('box should have margin props', () => {
   rerender(<Box margin="medium" />);
 
   expect(container.firstChild).toHaveStyle('margin: 1rem');
+});
+
+test('has padding props', () => {
+  const { container, rerender } = render(<Box />);
+
+  expect(container.firstChild).not.toHaveStyle('padding: 1rem');
+
+  rerender(<Box padding="medium" />);
+
+  expect(container.firstChild).toHaveStyle('padding: 1rem');
+});
+
+test('has backgroundColor props', () => {
+  const { container, rerender } = render(<Box />);
+
+  expect(container.firstChild).not.toHaveStyle('background-color: #fff');
+
+  rerender(<Box backgroundColor="white" />);
+
+  expect(container.firstChild).toHaveStyle('background-color: #fff');
+});
+
+test('has borderRadius props', () => {
+  const { container, rerender } = render(<Box />);
+
+  expect(container.firstChild).not.toHaveStyle('border-radius: 50%');
+
+  rerender(<Box borderRadius="circle" />);
+
+  expect(container.firstChild).toHaveStyle('border-radius: 50%');
+});
+
+test('has border props', () => {
+  const { container } = render(<Box border="box" />);
+
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('has elevation props', () => {
+  const { container } = render(<Box elevation="floating" />);
+
+  expect(container.firstChild).toMatchSnapshot();
 });
