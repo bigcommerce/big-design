ead0a798c4f7bc9ee4cc9e19fea7cc34308e7c43
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-03-25 14:26:49 -0500

    feat(story): box stories

---
+++ packages/storybook/stories/Box.story.tsx
@@ -0,0 +1,28 @@
+import { Box } from '@bigcommerce/big-design';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+storiesOf('Box', module)
+  .add('Basic', () => <Box>I'm a Box</Box>)
+  .add('With Border', () => (
+    <>
+      <Box border="box" margin="medium">
+        With Border box
+      </Box>
+      <Box border="box" borderRadius="normal" margin="medium">
+        With Border Radius
+      </Box>
+    </>
+  ))
+  .add('With Margin', () => <Box margin="medium">With Margin</Box>)
+  .add('With Background', () => <Box backgroundColor="primary30">With Background Color</Box>)
+  .add('With Elevation', () => (
+    <>
+      <Box elevation="floating" margin="medium">
+        Floating
+      </Box>
+      <Box elevation="raised" margin="medium">
+        Raised
+      </Box>
+    </>
+  ));
