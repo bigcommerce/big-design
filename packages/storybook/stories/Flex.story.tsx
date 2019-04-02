d46831d72103a3239e13d89eacc93f8ae924a188
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-04-02 08:06:02 -0500

    feat(story): flex stories

---
+++ packages/storybook/stories/Flex.story.tsx
@@ -0,0 +1,20 @@
+import { Box, Flex } from '@bigcommerce/big-design';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+storiesOf('Flex', module).add('Overview', () => (
+  <>
+    <Flex justifyContent="space-around" backgroundColor="warning30">
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+    </Flex>
+    <Flex flexDirection="column" backgroundColor="success30">
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+      <Box backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
+    </Flex>
+  </>
+));
