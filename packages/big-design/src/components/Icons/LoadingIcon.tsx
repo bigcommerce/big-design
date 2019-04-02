9bb6a8bcbe21fb9db1a3a705b783774cb11664db
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-04-02 08:20:44 -0500

    feat(component): icon components

---
+++ packages/big-design/src/components/Icons/LoadingIcon.tsx
@@ -0,0 +1,25 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React from 'react';
+
+import { Icon } from './Icon';
+
+export default class SvgLoadingIcon extends Icon {
+  render() {
+    const { title, theme, ...rest } = this.props;
+    const size = this.getSize();
+    const style = {
+      width: size,
+      height: size,
+    };
+    const props = { ...rest, style };
+
+    return (
+      <svg width={size} height={size} viewBox="0 0 20 20" fill="none" {...props}>
+        <title>{title}</title>
+        <path d="M18 10a8 8 0 1 0-8 8v-2.29A5.709 5.709 0 1 1 15.71 10H18z" fill="currentColor" />
+      </svg>
+    );
+  }
+}
---
+++ packages/big-design/src/components/Icons/PlusIcon.tsx
@@ -0,0 +1,28 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React from 'react';
+
+import { Icon } from './Icon';
+
+export default class SvgPlusIcon extends Icon {
+  render() {
+    const { title, theme, ...rest } = this.props;
+    const size = this.getSize();
+    const style = {
+      width: size,
+      height: size,
+    };
+    const props = { ...rest, style };
+
+    return (
+      <svg width={size} height={size} viewBox="0 0 20 20" fill="none" {...props}>
+        <title>{title}</title>
+        <path
+          d="M16 11h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H4c-.55 0-1-.45-1-1s.45-1 1-1h5V4c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"
+          fill="currentColor"
+        />
+      </svg>
+    );
+  }
+}
