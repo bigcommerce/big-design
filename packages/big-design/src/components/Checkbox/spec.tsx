86454d732fdda6023071bd7825662d78e82ec74a
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-04-08 08:48:02 -0500

    feat(component): checkbox component

---
+++ packages/big-design/src/components/Checkbox/spec.tsx
@@ -0,0 +1,82 @@
+import 'jest-styled-components';
+import React from 'react';
+import { fireEvent, render } from 'react-testing-library';
+
+import { Checkbox } from './index';
+import { StyleableCheckbox } from './private';
+
+test('render Checkbox (checked)', () => {
+  const { container } = render(<Checkbox label="Checked" checked={true} onChange={() => null} />);
+
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('render Checkbox (unchecked)', () => {
+  const { container } = render(<Checkbox label="Unchecked" checked={false} onChange={() => null} />);
+
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('has correct value for checked', () => {
+  const { getByTestId } = render(
+    <Checkbox label="Checked" checked={true} onChange={() => null} data-testid="checkbox" />,
+  );
+  const input = getByTestId('checkbox') as HTMLInputElement;
+
+  expect(input.checked).toBe(true);
+});
+
+test('has correct value for unchecked', () => {
+  const { getByTestId } = render(
+    <Checkbox label="Checked" checked={false} onChange={() => null} data-testid="checkbox" />,
+  );
+  const input = getByTestId('checkbox') as HTMLInputElement;
+
+  expect(input.checked).toBe(false);
+});
+
+test('triggers onChange when clicking the checkbox', () => {
+  const onChange = jest.fn();
+  const { getByTestId } = render(
+    <Checkbox label="Checked" checked={true} onChange={onChange} data-testid="checkbox" />,
+  );
+  const checkbox = getByTestId('checkbox') as HTMLInputElement;
+
+  fireEvent.click(checkbox);
+
+  expect(onChange).toHaveBeenCalled();
+});
+
+test('triggers onChange when clicking styled and text label', () => {
+  const onChange = jest.fn();
+  const { container } = render(<Checkbox label="Checked" checked={true} onChange={onChange} data-testid="checkbox" />);
+
+  const labels = container.querySelectorAll('label');
+
+  labels.forEach(label => fireEvent.click(label));
+
+  expect(onChange).toHaveBeenCalledTimes(2);
+});
+
+test('forwards ref', () => {
+  const ref = React.createRef<HTMLInputElement>();
+
+  const { container } = render(<Checkbox ref={ref} label="Checked" data-testid="checkbox" />);
+  const input = container.querySelector('input');
+
+  expect(input).toBe(ref.current);
+});
+
+test('does not forward styles', () => {
+  const { container } = render(<Checkbox label="Checked" className="test" style={{ background: 'red' }} />);
+
+  expect(container.getElementsByClassName('test').length).toBe(0);
+  expect(container.firstChild).not.toHaveStyle('background: red');
+});
+
+test('private StyleableCheckbox forwards styles', () => {
+  const { container } = render(<StyleableCheckbox label="Checked" className="test" style={{ background: 'red' }} />);
+
+  expect(container.getElementsByClassName('test').length).toBe(1);
+  expect(container.firstChild).toHaveStyle('background: red');
+});
---
+++ packages/big-design/src/components/Checkbox/styled.tsx
@@ -0,0 +1,51 @@
+import { hideVisually } from 'polished';
+import styled, { DefaultTheme, StyledComponent } from 'styled-components';
+
+import { defaultTheme } from '../../theme';
+import { StyleableText } from '../Typography/private';
+
+interface StyledCheckboxProps {
+  checked?: boolean;
+}
+
+export const CheckboxContainer = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export const HiddenCheckbox = styled.input`
+  ${hideVisually()}
+`;
+
+export const StyledCheckbox = styled.label<StyledCheckboxProps>`
+  ${({ theme }) => theme.border.box};
+  ${({ theme }) => theme.borderRadius.normal};
+
+  align-items: center;
+  background: ${props => (props.checked ? props.theme.colors.primary40 : props.theme.colors.white)};
+  border-color: ${props => (props.checked ? props.theme.colors.primary40 : props.theme.colors.secondary30)};
+  color: ${({ theme }) => theme.colors.white};
+  display: inline-flex;
+  height: ${({ theme }) => theme.spacing.large};
+  justify-content: center;
+  transition: all 150ms;
+  user-select: none;
+  width: ${({ theme }) => theme.spacing.large};
+
+  ${HiddenCheckbox}:focus + & {
+    box-shadow: ${({ theme }) => `0 0 0 ${theme.spacing.xxSmall} ${theme.colors.primary20}`};
+  }
+
+  svg {
+    visibility: ${props => (props.checked ? 'visible' : 'hidden')};
+  }
+`;
+
+export const StyledLabel = styled(StyleableText).attrs({
+  as: 'label',
+})<React.LabelHTMLAttributes<HTMLLabelElement>>`
+  margin-left: ${({ theme }) => theme.spacing.medium};
+` as StyledComponent<'label', DefaultTheme>;
+
+StyledCheckbox.defaultProps = { theme: defaultTheme };
+StyledLabel.defaultProps = { theme: defaultTheme };
