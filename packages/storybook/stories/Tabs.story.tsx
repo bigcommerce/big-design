dafb6b6d3910a8549ef36156029f3d9f0af50770
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-04-08 09:11:28 -0500

    feat(story): tabs stories

---
+++ packages/storybook/stories/Tabs.story.tsx
@@ -0,0 +1,42 @@
+import { Box, Panel, Tabs, Text } from '@bigcommerce/big-design';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+interface State {
+  activeTab: string;
+}
+
+class TabsStory extends React.PureComponent<{}, State> {
+  state = {
+    activeTab: 'tab1',
+  };
+
+  render() {
+    const { activeTab } = this.state;
+
+    return (
+      <>
+        <Tabs activeTab={activeTab} onTabClick={clickedTab => this.setState({ activeTab: clickedTab })}>
+          <Tabs.Tab id="tab1">Example 1</Tabs.Tab>
+          <Tabs.Tab id="tab2">Example 2</Tabs.Tab>
+          <Tabs.Tab id="tab3">Example 3</Tabs.Tab>
+          <Tabs.Tab id="tab4" disabled>
+            Example 4
+          </Tabs.Tab>
+        </Tabs>
+        <Box marginTop="large">
+          {activeTab === 'tab1' && <Text>Content 1</Text>}
+          {activeTab === 'tab2' && <Text>Content 2</Text>}
+          {activeTab === 'tab3' && <Text>Content 3</Text>}
+          {activeTab === 'tab4' && <Text>Content 4</Text>}
+        </Box>
+      </>
+    );
+  }
+}
+
+storiesOf('Tabs', module).add('Overview', () => (
+  <Panel margin="xxxLarge">
+    <TabsStory />
+  </Panel>
+));
