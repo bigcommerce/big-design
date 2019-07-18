9c9dbd481293e6b862eba143b97e13cb074943bb
Chancellor Clark <chancellor.clark@bigcommerce.com>
2019-07-18 15:30:18 -0500

    feat: refactor buttons story per discussion

--- packages/storybook/stories/Button/Button.story.tsx
+++ packages/storybook/stories/Button/Button.story.tsx
@@ -1,13 +1,20 @@
-import { Button, DropdownIcon, H0, H1, H2, Link, PlusIcon, Text } from '@bigcommerce/big-design';
-import { linkTo } from '@storybook/addon-links';
+import { Button, DropdownIcon, Grid, H0, H1, H2, Link, PlusIcon, Text } from '@bigcommerce/big-design';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
-import { CodePreview, CodeSnippet, List, PropTable } from '../components';
+import { CodePreview, CodeSnippet } from '../../components';
+
+import { ButtonPropTable } from './';
 
 storiesOf('Button', module).add('Overview', () => (
   <>
     <H0>Buttons</H0>
+
+    <Text>
+      Buttons are calls to action used throughout the product experience.{' '}
+      <Link href="http://bigcommerce.design/buttons">Buttons Design Guidelines</Link>
+    </Text>
+
     <CodePreview>
       {/* jsx-to-string:start */}
       <Button actionType="normal" isLoading={false} variant="primary">
@@ -15,62 +22,25 @@ storiesOf('Button', module).add('Overview', () => (
       </Button>
       {/* jsx-to-string:end */}
     </CodePreview>
-    <Text>Buttons are calls to action used throughout the product experience.</Text>
-    <List>
-      <List.Item>Buttons provide a visual indication that users can take a specified action.</List.Item>
-      <List.Item>Buttons should be clear and direct.</List.Item>
-      <List.Item>Keep button labels short and concise.</List.Item>
-      <List.Item>Button labels must remain on one line.</List.Item>
-    </List>
+
+    <H1>API</H1>
+
+    <ButtonPropTable />
 
     <H1>Variants</H1>
 
-    <H2>Primary</H2>
-
     <Text>
-      There should only be one primary button one per page. It is used to describe the most important action on the
-      page.
-    </Text>
-
-    <Text>
-      <b>Use Case:</b> A primary button is used to "Create a New Product".
+      There are three types of variants to choose from: <code>primary</code>, <code>secondary</code>, and{' '}
+      <code>subtle</code>. You can determine what type of variant by using the <code>variant</code> prop.
     </Text>
 
     <CodePreview>
       {/* jsx-to-string:start */}
-      <Button variant="primary">Primary</Button>
-      {/* jsx-to-string:end */}
-    </CodePreview>
-
-    <H2>Secondary</H2>
-
-    <Text>
-      There can be several secondary actions per page. Can be used in conjunction with a primary action or
-      independently.
-    </Text>
-
-    <Text>
-      <b>Use Case:</b> A secondary button used in conjunction with a primary button. In Store Design: “Publish” =
-      secondary button. “Save” = primary button.
-    </Text>
-
-    <CodePreview>
-      {/* jsx-to-string:start */}
-      <Button variant="secondary">Secondary</Button>
-      {/* jsx-to-string:end */}
-    </CodePreview>
-
-    <H2>Subtle</H2>
-
-    <Text>Subtle actions are used in together with other actions rather than independently.</Text>
-
-    <Text>
-      <b>Use Case:</b> Used as a "Cancel" action inside <Link onClick={linkTo('Modal') as any}>Modals</Link>.
-    </Text>
-
-    <CodePreview>
-      {/* jsx-to-string:start */}
-      <Button variant="subtle">Subtle</Button>
+      <Grid columns="repeat(3, min-content)">
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="subtle">Subtle</Button>
+      </Grid>
       {/* jsx-to-string:end */}
     </CodePreview>
 
@@ -83,12 +53,10 @@ storiesOf('Button', module).add('Overview', () => (
 
     <CodePreview>
       {/* jsx-to-string:start */}
-      <>
-        <Button actionType="normal" marginRight="xSmall">
-          Normal
-        </Button>
+      <Grid columns="repeat(2, min-content)">
+        <Button actionType="normal">Normal</Button>
         <Button actionType="destructive">Destructive</Button>
-      </>
+      </Grid>
       {/* jsx-to-string:end */}
     </CodePreview>
 
@@ -147,7 +115,15 @@ storiesOf('Button', module).add('Overview', () => (
 
     <CodePreview>
       {/* jsx-to-string:start */}
-      <Button disabled>Disabled</Button>
+      <Grid columns="repeat(3, min-content)">
+        <Button disabled>Disabled</Button>
+        <Button disabled variant="secondary">
+          Disabled
+        </Button>
+        <Button disabled variant="subtle">
+          Disabled
+        </Button>
+      </Grid>
       {/* jsx-to-string:end */}
     </CodePreview>
 
@@ -160,51 +136,15 @@ storiesOf('Button', module).add('Overview', () => (
 
     <CodePreview>
       {/* jsx-to-string:start */}
-      <>
-        <Button iconOnly={<PlusIcon title="add" />} marginRight="xSmall" />
-        <Button iconLeft={<PlusIcon />} marginRight="xSmall">
+      <Grid columns="repeat(4, min-content)">
+        <Button iconOnly={<PlusIcon title="add" />} />
+        <Button iconLeft={<PlusIcon />}>Label</Button>
+        <Button iconLeft={<PlusIcon />} iconRight={<DropdownIcon />}>
           Label
         </Button>
-        <Button iconLeft={<PlusIcon />} iconRight={<DropdownIcon />} marginRight="xSmall">
-          Label
-        </Button>
-        <Button iconRight={<DropdownIcon />} marginRight="xSmall">
-          Label
-        </Button>
-      </>
+        <Button iconRight={<DropdownIcon />}>Label</Button>
+      </Grid>
       {/* jsx-to-string:end */}
     </CodePreview>
-
-    <H1>API</H1>
-
-    <PropTable>
-      <PropTable.Prop name="actionType" types={['normal', 'destructive']} defaults="normal">
-        Indicates whether your button's action is of normal or destructive nature.
-      </PropTable.Prop>
-      <PropTable.Prop name="iconLeft" types={<Link onClick={linkTo('Icons') as any}>Icon</Link>} defaults="">
-        Pass in an <Link onClick={linkTo('Icons') as any}>Icon</Link> component to display to the left of the text.
-      </PropTable.Prop>
-      <PropTable.Prop name="iconOnly" types={<Link onClick={linkTo('Icons') as any}>Icon</Link>} defaults="">
-        Pass in an <Link onClick={linkTo('Icons') as any}>Icon</Link> component to replace content with an icon.
-      </PropTable.Prop>
-      <PropTable.Prop name="iconRight" types={<Link onClick={linkTo('Icons') as any}>Icon</Link>} defaults="">
-        Pass in an <Link onClick={linkTo('Icons') as any}>Icon</Link> component to display to the right of the text.
-      </PropTable.Prop>
-      <PropTable.Prop name="isLoading" types="boolean" defaults="false">
-        Used to determine if component is in a loading state.
-      </PropTable.Prop>
-      <PropTable.Prop
-        name="spinner"
-        types={<Link onClick={linkTo('Spinner') as any}>Spinner</Link>}
-        defaults="<Spinner overlay={false} />"
-      >
-        Add a custom spinner to view when <code>isLoading</code> is <code>true</code>
-      </PropTable.Prop>
-      <PropTable.Prop name="variant" types={['primary', 'secondary', 'subtle']} defaults="primary">
-        Determines which style of button to display.
-      </PropTable.Prop>
-    </PropTable>
-
-    <Link href="http://bigcommerce.design/buttons">Buttons Design Guidelines</Link>
   </>
 ));
