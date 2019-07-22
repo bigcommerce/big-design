f97b2d88f49744cf1ad3f344610553cd71f0b8fa
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-07-22 14:34:03 -0500

    docs(story): add colors documentation

---
+++ packages/storybook/stories/Colors/styled.tsx
@@ -0,0 +1,7 @@
+import { remCalc, Box } from '@bigcommerce/big-design';
+import styled from 'styled-components';
+
+export const StyledColor = styled(Box)`
+  height: ${remCalc(30)};
+  width: ${remCalc(300)};
+`;
