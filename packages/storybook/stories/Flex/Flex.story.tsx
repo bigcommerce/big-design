23a60ed727a5004240b7603be3040e6cc98c4873
Chancellor Clark <chancellor.clark@bigcommerce.com>
2019-07-22 13:59:01 -0500

    feat: add examples to flex story

--- packages/storybook/stories/Flex/Flex.story.tsx
+++ packages/storybook/stories/Flex/Flex.story.tsx
@@ -1,24 +1,96 @@
-import { Flex } from '@bigcommerce/big-design';
+import { Box, Flex, H0, H1, H2, Text } from '@bigcommerce/big-design';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
+import { CodePreview } from '../../components';
+
 import { FlexItemPropTable, FlexPropTable } from './';
+
+const ExampleBox: React.FC<{ num: string }> = ({ num }) => (
+  <Box backgroundColor="secondary10" border="box" marginHorizontal="xSmall" padding="small">
+    Item {num}
+  </Box>
+);
 
 storiesOf('Flex', module).add('Overview', () => (
   <>
-    <Flex justifyContent="space-around" backgroundColor="warning30">
-      <Flex.Item backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
-      <Flex.Item backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
-      <Flex.Item backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
-      <Flex.Item backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
-    </Flex>
-    <Flex direction="column" backgroundColor="success30">
-      <Flex.Item backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} />
-      <Flex.Item backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} alignSelf="center" />
-      <Flex.Item backgroundColor="primary" margin="small" style={{ height: 50, width: 50 }} alignSelf="flex-end" />
-    </Flex>
+    <H0>Flex</H0>
+
+    <Text>A flex container used for customizable layouts.</Text>
+
+    <CodePreview scope={{ ExampleBox }}>
+      {/* jsx-to-string:start */}
+      <Flex alignContent="stretch" alignItems="stretch" direction="row" justifyContent="flex-start" wrap="nowrap">
+        <Flex.Item alignSelf="auto" basis="auto" grow={0} order={0} shrink={1}>
+          <ExampleBox num="1" />
+        </Flex.Item>
+        <Flex.Item alignSelf="auto" basis="auto" grow={0} order={0} shrink={1}>
+          <ExampleBox num="2" />
+        </Flex.Item>
+        <Flex.Item alignSelf="auto" basis="auto" grow={0} order={0} shrink={1}>
+          <ExampleBox num="3" />
+        </Flex.Item>
+        <Flex.Item alignSelf="auto" basis="auto" grow={0} order={0} shrink={1}>
+          <ExampleBox num="4" />
+        </Flex.Item>
+      </Flex>
+      {/* jsx-to-string:end */}
+    </CodePreview>
+
+    <H1>API</H1>
+
+    <H2>Flex</H2>
 
     <FlexPropTable />
+
+    <H2>Flex.Item</H2>
+
     <FlexItemPropTable />
+
+    <H1>Examples</H1>
+
+    <Text>{/* Something */}</Text>
+
+    <CodePreview scope={{ ExampleBox }}>
+      {/* jsx-to-string:start */}
+      <Flex justifyContent="space-between">
+        <Flex.Item>
+          <ExampleBox num="1" />
+        </Flex.Item>
+        <Flex.Item>
+          <ExampleBox num="2" />
+        </Flex.Item>
+        <Flex.Item>
+          <ExampleBox num="3" />
+        </Flex.Item>
+        <Flex.Item>
+          <ExampleBox num="4" />
+        </Flex.Item>
+      </Flex>
+      {/* jsx-to-string:end */}
+    </CodePreview>
+
+    <Text>{/* Something */}</Text>
+
+    <CodePreview scope={{ ExampleBox }}>
+      {/* jsx-to-string:start */}
+      <Flex wrap="wrap">
+        <Flex.Item basis="100%">
+          <ExampleBox num="1" />
+        </Flex.Item>
+        <Flex.Item basis="100%">
+          <ExampleBox num="2" />
+        </Flex.Item>
+        <Flex.Item basis="100%">
+          <ExampleBox num="3" />
+        </Flex.Item>
+        <Flex.Item basis="100%">
+          <ExampleBox num="4" />
+        </Flex.Item>
+      </Flex>
+      {/* jsx-to-string:end */}
+    </CodePreview>
+
+    {/* Grow, Order, Space-Between, Basis+Wrap */}
   </>
 ));
