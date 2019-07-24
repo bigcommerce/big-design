b477b95c2e566c86edefa2da7f6bc1f1bdeefad6
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-07-24 13:24:07 -0500

    refactor(configs): share babel and rollup configs

---
+++ packages/configs/rollup/rollup.config.js
@@ -0,0 +1,63 @@
+const { tmpdir } = require('os');
+const { DEFAULT_EXTENSIONS } = require('@babel/core');
+const babel = require('rollup-plugin-babel');
+const commonjs = require('rollup-plugin-commonjs');
+const filesize = require('rollup-plugin-filesize');
+const resolve = require('rollup-plugin-node-resolve');
+const replace = require('rollup-plugin-replace');
+const sourceMaps = require('rollup-plugin-sourcemaps');
+const typescript = require('rollup-plugin-typescript2');
+
+const nodeEnv = process.env.NODE_ENV || 'production';
+
+function generateConfig(pkg) {
+  const externals = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ];
+
+  return {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
+    ],
+    external: makeExternalPredicate(externals),
+    watch: {
+      include: 'src/**'
+    },
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(nodeEnv)
+      }),
+      typescript({
+        cacheRoot: tmpdir(),
+        check: nodeEnv === 'production',
+        typescript: require('typescript'),
+        useTsconfigDeclarationDir: true
+      }),
+      babel({
+        extensions: [...DEFAULT_EXTENSIONS, 'ts', 'tsx'],
+        exclude: 'node_modules/**'
+      }),
+      commonjs(),
+      resolve(),
+      sourceMaps(),
+      filesize()
+    ]
+  };
+}
+
+function makeExternalPredicate(externalsArr) {
+  if (externalsArr.length === 0) {
+    return () => false;
+  }
+
+  const externalPattern = new RegExp(`^(${externalsArr.join('|')})($|/)`);
+
+  return id => externalPattern.test(id);
+}
+
+module.exports = {
+  generateConfig
+};
