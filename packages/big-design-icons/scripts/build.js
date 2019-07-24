acfe8fe7e07e35c575ff76e2063b101036ca74bb
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-07-24 13:07:12 -0500

    feat(icons): initial package setup (#89)

---
+++ packages/big-design-icons/scripts/build.js
@@ -0,0 +1,54 @@
+const { default: svgr } = require('@svgr/core');
+const { promisify } = require('util');
+const rimraf = require('rimraf');
+const config = require('./svgr.config');
+const { outputFile, readFile } = require('fs-extra');
+const { basename, join } = require('path');
+const glob = require('glob-promise');
+const camelcase = require('camelcase');
+
+const SOURCE = join(__dirname, '..', 'svgs', '*', '*.svg');
+const DEST_PATH = join(__dirname, '..', 'src', 'components');
+
+const componentNames = new Set();
+const asyncRimraf = promisify(rimraf);
+
+async function convertToReactComponent(filePath, iconName) {
+  const svgCode = await readFile(filePath, 'utf8');
+  const destPath = join(DEST_PATH, `${iconName}.tsx`);
+  const code = await svgr(svgCode, config, { componentName: iconName });
+
+  return outputFile(destPath, code);
+}
+
+async function generateIcons() {
+  const iconFiles = await glob(SOURCE);
+
+  return Promise.all(
+    iconFiles.map(iconFilePath => {
+      const filename = basename(iconFilePath, '.svg');
+      const name = `Icon${camelcase(filename, { pascalCase: true })}`;
+
+      componentNames.add(name);
+
+      console.log(`Building: ${name}`);
+
+      return convertToReactComponent(iconFilePath, name);
+    }),
+  );
+}
+
+function cleanDestDirectory() {
+  return asyncRimraf(DEST_PATH);
+}
+
+(async () => {
+  await cleanDestDirectory();
+  await generateIcons();
+
+  const indexFile = Array.from(componentNames).map(name => `export * from './${name}';`);
+
+  await outputFile(join(DEST_PATH, 'index.ts'), indexFile.join('\n'));
+
+  console.log('Done!');
+})();
