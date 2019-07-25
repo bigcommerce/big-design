243d1a2fa25a8ebcfe3241bb0782a8ce3d6a0f9e
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-07-25 08:27:02 -0500

    feat(icons): add initial batch of icons

---
+++ packages/big-design-icons/src/components/IconDelete.tsx
@@ -0,0 +1,16 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React from 'react';
+
+import { createStyledIcon, IconProps } from '../base';
+
+const Icon = React.memo<Partial<IconProps>>(({ title, ...props }) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" stroke="currentColor" fill="currentColor" strokeWidth="0" {...props}>
+    <title>{title}</title>
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z" />
+  </svg>
+));
+
+export const IconDelete = createStyledIcon(Icon);
---
+++ packages/big-design-icons/src/components/IconFilterList.tsx
@@ -0,0 +1,16 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React from 'react';
+
+import { createStyledIcon, IconProps } from '../base';
+
+const Icon = React.memo<Partial<IconProps>>(({ title, ...props }) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" stroke="currentColor" fill="currentColor" strokeWidth="0" {...props}>
+    <title>{title}</title>
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path d="M11 18h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1zm4 6h10c.55 0 1-.45 1-1s-.45-1-1-1H7c-.55 0-1 .45-1 1s.45 1 1 1z" />
+  </svg>
+));
+
+export const IconFilterList = createStyledIcon(Icon);
