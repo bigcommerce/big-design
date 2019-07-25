03772fbdb92e047327917bd120cda54b86068cf1
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-07-25 12:02:32 -0500

    feat(icons): use Icon postfix (#97)

---
+++ packages/big-design-icons/src/components/CheckCircleIcon.tsx
@@ -0,0 +1,16 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React from 'react';
+
+import { createStyledIcon, IconProps } from '../base';
+
+const Icon = React.memo<Partial<IconProps>>(({ title, ...props }) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" stroke="currentColor" fill="currentColor" strokeWidth="0" {...props}>
+    <title>{title}</title>
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.29 16.29L5.7 12.7a.996.996 0 111.41-1.41L10 14.17l6.88-6.88a.996.996 0 111.41 1.41l-7.59 7.59a.996.996 0 01-1.41 0z" />
+  </svg>
+));
+
+export const CheckCircleIcon = createStyledIcon(Icon);
---
+++ packages/big-design-icons/src/components/VisibilityIcon.tsx
@@ -0,0 +1,16 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React from 'react';
+
+import { createStyledIcon, IconProps } from '../base';
+
+const Icon = React.memo<Partial<IconProps>>(({ title, ...props }) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" stroke="currentColor" fill="currentColor" strokeWidth="0" {...props}>
+    <title>{title}</title>
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path d="M12 4C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
+  </svg>
+));
+
+export const VisibilityIcon = createStyledIcon(Icon);
