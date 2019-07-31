7f24cd8737f71a9e1d35f2b1bf219be84b22a598
Chancellor Clark <chancellor.clark@bigcommerce.com>
2019-07-31 14:47:04 -0500

    refactor: move flexed item props into same file

--- packages/big-design/src/mixins/flex/flex.ts
+++ packages/big-design/src/mixins/flex/flex.ts
@@ -1,18 +1,90 @@
 import { breakpointsOrder, Breakpoints, ThemeInterface } from '@bigcommerce/big-design-theme';
 import { css, FlattenSimpleInterpolation } from 'styled-components';
 
-import {
-  AlignContent,
-  AlignItems,
-  AlignSelf,
-  FlexBasis,
-  FlexDirection,
-  FlexGrow,
-  FlexOrder,
-  FlexShrink,
-  FlexWrap,
-  JustifyContent,
-} from './props';
+import { Responsive } from '../../types';
+
+type SingleAlignContent =
+  | 'normal'
+  | 'start'
+  | 'end'
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'baseline'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+  | 'stretch';
+type ResponsiveAlignContent = Responsive<SingleAlignContent>;
+type AlignContent = SingleAlignContent | ResponsiveAlignContent;
+
+type SingleAlignItems =
+  | 'normal'
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'start'
+  | 'end'
+  | 'self-start'
+  | 'self-end'
+  | 'baseline'
+  | 'stretch';
+type ResponsiveAlignItems = Responsive<SingleAlignItems>;
+type AlignItems = SingleAlignItems | ResponsiveAlignItems;
+
+type SingleAlignSelf =
+  | 'auto'
+  | 'normal'
+  | 'self-start'
+  | 'self-end'
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'baseline'
+  | 'stretch';
+type ResponsiveAlignSelf = Responsive<SingleAlignSelf>;
+type AlignSelf = SingleAlignSelf | ResponsiveAlignSelf;
+
+type SingleFlexDirection = 'row' | 'column' | 'row-reverse' | 'column-reverse';
+type ResponsiveFlexDirection = Responsive<SingleFlexDirection>;
+type FlexDirection = SingleFlexDirection | ResponsiveFlexDirection;
+
+type SingleFlexWrap = 'nowrap' | 'wrap' | 'wrap-reversed';
+type ResponsiveFlexWrap = Responsive<SingleFlexWrap>;
+type FlexWrap = SingleFlexWrap | ResponsiveFlexWrap;
+
+type SingleJustifyContent =
+  | 'normal'
+  | 'start'
+  | 'end'
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'left'
+  | 'right'
+  | 'baseline'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+  | 'stretch';
+type ResponsiveJustifyContent = Responsive<SingleJustifyContent>;
+type JustifyContent = SingleJustifyContent | ResponsiveJustifyContent;
+
+type SingleFlexBasis = 'auto' | 'fill' | 'min-content' | 'max-content' | 'fit-content' | 'content' | string;
+type ResponsiveFlexBasis = Responsive<SingleFlexBasis>;
+type FlexBasis = SingleFlexBasis | ResponsiveFlexBasis;
+
+type SingleFlexGrow = number;
+type ResponsiveFlexGrow = Responsive<SingleFlexGrow>;
+type FlexGrow = SingleFlexGrow | ResponsiveFlexGrow;
+
+type SingleFlexOrder = number;
+type ResponsiveFlexOrder = Responsive<SingleFlexOrder>;
+type FlexOrder = SingleFlexOrder | ResponsiveFlexOrder;
+
+type SingleFlexShrink = number;
+type ResponsiveFlexShrink = Responsive<SingleFlexShrink>;
+type FlexShrink = SingleFlexShrink | ResponsiveFlexShrink;
 
 export type FlexMixin = Partial<{
   alignContent: AlignContent;
