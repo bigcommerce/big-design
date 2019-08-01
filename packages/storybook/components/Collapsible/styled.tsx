4e58296beb26819603f91128dc181e263abd6f49
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-08-01 14:35:12 -0500

    feat(story): add bottomMargin to Collapsible when collapsed (#120)

--- packages/storybook/components/Collapsible/styled.tsx
+++ packages/storybook/components/Collapsible/styled.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 
 export const StyledFlex = styled(Flex)`
   cursor: pointer;
-  margin-bottom: ${({ theme }) => theme.spacing.small};
 `;
 
 StyledFlex.defaultProps = { theme: defaultTheme };
