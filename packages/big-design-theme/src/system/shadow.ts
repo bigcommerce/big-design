659c6ad151dd2cafdd1d9a2f7a5e2c4529ac7c10
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-08-05 14:28:28 -0500

    feat(theme): rename elevation to shadow
    BREAKING CHANGE: `elevation` is now called `shadow`

--- packages/big-design-theme/src/system/shadow.ts
+++ packages/big-design-theme/src/system/shadow.ts
@@ -3,12 +3,12 @@ import { css } from 'styled-components';
 import { createRGBA } from '../helpers';
 import { AllStyleInterpolations } from '../styled/types';
 
-export interface Elevation {
+export interface Shadow {
   floating: AllStyleInterpolations;
   raised: AllStyleInterpolations;
 }
 
-export const elevation: Elevation = {
+export const shadow: Shadow = {
   floating: css`
     border-radius: ${({ theme }) => theme.borderRadius.normal};
     box-shadow: ${({ theme }) => `0px 2px 12px ${createRGBA(theme.colors.secondary70, 0.2)}`};
