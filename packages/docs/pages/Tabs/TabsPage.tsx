ee757686fa718b76804eb7cc4203839fa9c11423
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-08-08 12:07:51 -0500

    feat(docs): tabs documentation (#136)

--- packages/docs/pages/Tabs/TabsPage.tsx
+++ packages/docs/pages/Tabs/TabsPage.tsx
@@ -1,33 +1,51 @@
-import { Box, Tabs, Text } from '@bigcommerce/big-design';
+import { Box, H0, H1, H2, Tabs, Text } from '@bigcommerce/big-design';
 import React from 'react';
 
-import { CodePreview } from '../../components';
+import { Code, CodePreview } from '../../components';
+import { TabsPropTable, TabPropTable } from '../../PropTables';
 
 export default () => (
-  <CodePreview>
-    {/* jsx-to-string:start */}
-    {function Example() {
-      const [activeTab, setActiveTab] = React.useState('tab1');
+  <>
+    <H0>Tabs</H0>
 
-      return (
-        <>
-          <Tabs activeTab={activeTab} onTabClick={setActiveTab}>
-            <Tabs.Tab id="tab1">Example 1</Tabs.Tab>
-            <Tabs.Tab id="tab2">Example 2</Tabs.Tab>
-            <Tabs.Tab id="tab3">Example 3</Tabs.Tab>
-            <Tabs.Tab id="tab4" disabled>
-              Example 4
-            </Tabs.Tab>
-          </Tabs>
-          <Box marginTop="large">
-            {activeTab === 'tab1' && <Text>Content 1</Text>}
-            {activeTab === 'tab2' && <Text>Content 2</Text>}
-            {activeTab === 'tab3' && <Text>Content 3</Text>}
-            {activeTab === 'tab4' && <Text>Content 4</Text>}
-          </Box>
-        </>
-      );
-    }}
-    {/* jsx-to-string:end */}
-  </CodePreview>
+    <Text>
+      The <Code primary>Tabs</Code> and <Code primary>Tabs.Tab</Code> components are used to organize and navigate
+      between content types that are related and at the same level of information architecture heirarchy.
+    </Text>
+
+    <CodePreview>
+      {/* jsx-to-string:start */}
+      {function Example() {
+        const [activeTab, setActiveTab] = React.useState('tab1');
+
+        return (
+          <>
+            <Tabs activeTab={activeTab} onTabClick={setActiveTab}>
+              <Tabs.Tab id="tab1">Example 1</Tabs.Tab>
+              <Tabs.Tab id="tab2">Example 2</Tabs.Tab>
+              <Tabs.Tab id="tab3">Example 3</Tabs.Tab>
+              <Tabs.Tab id="tab4" disabled>
+                Example 4
+              </Tabs.Tab>
+            </Tabs>
+            <Box marginTop="large">
+              {activeTab === 'tab1' && <Text>Content 1</Text>}
+              {activeTab === 'tab2' && <Text>Content 2</Text>}
+              {activeTab === 'tab3' && <Text>Content 3</Text>}
+              {activeTab === 'tab4' && <Text>Content 4</Text>}
+            </Box>
+          </>
+        );
+      }}
+      {/* jsx-to-string:end */}
+    </CodePreview>
+
+    <H1>API</H1>
+
+    <TabsPropTable />
+
+    <H2>Tabs.Tab</H2>
+
+    <TabPropTable />
+  </>
 );
