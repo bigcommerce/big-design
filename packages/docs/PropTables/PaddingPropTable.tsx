a27b1b5f14e3b8eca6ee5b13e42091abd31f39af
stevenbennitt <steven.bennett@bigcommerce.com>
2019-08-09 20:17:35 -0500

    Updated Props Links

--- packages/docs/PropTables/PaddingPropTable.tsx
+++ packages/docs/PropTables/PaddingPropTable.tsx
@@ -5,25 +5,25 @@ import { PropTable } from '../components';
 
 export const PaddingPropTable: React.FC = () => (
   <PropTable>
-    <PropTable.Prop name="padding" types={<Link href="/utilities/padding">Padding</Link>}>
+    <PropTable.Prop name="padding" types={<Link href="/big-design/utilities/padding">Padding</Link>}>
       Determines the padding to be applied.
     </PropTable.Prop>
-    <PropTable.Prop name="paddingTop" types={<Link href="/utilities/padding">Padding</Link>}>
+    <PropTable.Prop name="paddingTop" types={<Link href="/big-design/utilities/padding">Padding</Link>}>
       Determines the top padding to be applied.
     </PropTable.Prop>
-    <PropTable.Prop name="paddingRight" types={<Link href="/utilities/padding">Padding</Link>}>
+    <PropTable.Prop name="paddingRight" types={<Link href="/big-design/utilities/padding">Padding</Link>}>
       Determines the right padding to be applied.
     </PropTable.Prop>
-    <PropTable.Prop name="paddingBottom" types={<Link href="/utilities/padding">Padding</Link>}>
+    <PropTable.Prop name="paddingBottom" types={<Link href="/big-design/utilities/padding">Padding</Link>}>
       Determines the bottom padding to be applied.
     </PropTable.Prop>
-    <PropTable.Prop name="paddingLeft" types={<Link href="/utilities/padding">Padding</Link>}>
+    <PropTable.Prop name="paddingLeft" types={<Link href="/big-design/utilities/padding">Padding</Link>}>
       Determines the left padding to be applied.
     </PropTable.Prop>
-    <PropTable.Prop name="paddingVertical" types={<Link href="/utilities/padding">Padding</Link>}>
+    <PropTable.Prop name="paddingVertical" types={<Link href="/big-design/utilities/padding">Padding</Link>}>
       Determines the top and bottom padding to be applied.
     </PropTable.Prop>
-    <PropTable.Prop name="paddingHorizontal" types={<Link href="/utilities/padding">Padding</Link>}>
+    <PropTable.Prop name="paddingHorizontal" types={<Link href="/big-design/utilities/padding">Padding</Link>}>
       Determines the left and right padding to be applied.
     </PropTable.Prop>
   </PropTable>
