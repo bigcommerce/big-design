8a578dbf1e2119c08999db627b1b64e837d2fe74
Steven Bennett <steven.bennett@bigcommerce.com>
2019-08-12 14:31:50 -0500

    Revert "Updated Prop Links"
    This reverts commit c1242e50734202119457338615b9c3968b3599e7.

--- packages/docs/PropTables/IconPropTable.tsx
+++ packages/docs/PropTables/IconPropTable.tsx
@@ -5,11 +5,11 @@ import { PropTable } from '../components';
 
 export const IconPropTable: React.FC = () => (
   <PropTable>
-    <PropTable.Prop name="color" types={<Link href="/big-design/colors">Color</Link>}>
-      Sets the icon color given a color name from our <Link href="/big-design/colors">palette</Link>.
+    <PropTable.Prop name="color" types={<Link href="/colors">Color</Link>}>
+      Sets the icon color given a color name from our <Link href="/colors">palette</Link>.
     </PropTable.Prop>
-    <PropTable.Prop name="size" types={[<Link href="/big-design/utilities/spacing">Spacing</Link>, 'number']}>
-      Determines the size of the icon. Accepts a <Link href="/big-design/utilities/spacing">Spacing</Link> value or a number of px.
+    <PropTable.Prop name="size" types={[<Link href="/utilities/spacing">Spacing</Link>, 'number']}>
+      Determines the size of the icon. Accepts a <Link href="/utilities/spacing">Spacing</Link> value or a number of px.
     </PropTable.Prop>
   </PropTable>
 );
