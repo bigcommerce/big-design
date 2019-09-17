8a3139177a19771ff8977517ded72661a76122de
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-09-17 11:38:53 -0500

    fix(component): mock popper.js globally

--- packages/big-design/src/components/Dropdown/spec.tsx
+++ packages/big-design/src/components/Dropdown/spec.tsx
@@ -6,23 +6,6 @@ import { Button } from '../Button';
 
 import { Dropdown } from './Dropdown';
 
-jest.mock('popper.js', () => {
-  const PopperJS = jest.requireActual('popper.js');
-
-  return class {
-    static placements = PopperJS.placements;
-
-    constructor() {
-      return {
-        // tslint:disable-next-line: no-empty
-        destroy: () => {},
-        // tslint:disable-next-line: no-empty
-        scheduleUpdate: () => {},
-      };
-    }
-  };
-});
-
 const DropdownMock = (
   <Dropdown trigger={<Button>Button</Button>}>
     <Dropdown.Item value={0}>Option</Dropdown.Item>
