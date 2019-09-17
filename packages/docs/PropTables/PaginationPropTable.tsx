b9effd41644c06be41d01f5f4468c5345eaf93e7
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-09-17 11:39:40 -0500

    feat(docs): end sentences with dots on pagination props

--- packages/docs/PropTables/PaginationPropTable.tsx
+++ packages/docs/PropTables/PaginationPropTable.tsx
@@ -6,22 +6,22 @@ export const PaginationPropTable: React.FC = () => {
   return (
     <PropTable>
       <PropTable.Prop name="itemsPerPage" types="number" defaults="" required>
-        Indicates how many items are displayed per page
+        Indicates how many items are displayed per page.
       </PropTable.Prop>
       <PropTable.Prop name="currentPage" types="number" defaults="" required>
-        Indicates the page currently/initially displayed
+        Indicates the page currently/initially displayed.
       </PropTable.Prop>
       <PropTable.Prop name="totalItems" types="number" defaults="" required>
-        Indicates how many items in total will be displayed
+        Indicates how many items in total will be displayed.
       </PropTable.Prop>
       <PropTable.Prop name="itemsPerPageOptions" types="number[]" defaults="" required>
-        Indicates options for per-page ranges
+        Indicates options for per-page ranges.
       </PropTable.Prop>
       <PropTable.Prop name="onPageChange" types="(page: number) => void" required>
-        Function that will be called when a navigation arrow is clicked
+        Function that will be called when a navigation arrow is clicked.
       </PropTable.Prop>
       <PropTable.Prop name="onItemsPerPageChange" types="(range: number) => void" required>
-        Function that will be called when a new per-page range is selected
+        Function that will be called when a new per-page range is selected.
       </PropTable.Prop>
     </PropTable>
   );
