5187245eda7294b3e2019c5d9a51bc16890f57bd
Chancellor Clark <chancellor.clark@bigcommerce.com>
2019-09-24 10:45:45 -0500

    fix(component): add missing hover rows on Table component (#205)

--- packages/big-design/src/components/Table/Row/styled.tsx
+++ packages/big-design/src/components/Table/Row/styled.tsx
@@ -6,6 +6,10 @@ import { TableRowProps } from './Row';
 export const StyledTableRow = styled.tr<TableRowProps>`
   background-color: ${({ selected, theme }) => (selected ? theme.colors.primary10 : 'transparent')};
 
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary10};
+  }
+
   ${props =>
     props.verticalAlign &&
     css`
