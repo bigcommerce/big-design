8a90002bea6467a48ff98a7be5bfa269042fb4e6
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-10-15 18:32:26 -0500

    feat(component): add header and action props to Panel

--- packages/big-design/src/components/Panel/Panel.tsx
+++ packages/big-design/src/components/Panel/Panel.tsx
@@ -1,19 +1,78 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import { MarginProps } from '../../mixins';
+import { Button, ButtonProps } from '../Button';
+import { Flex } from '../Flex';
+import { H2 } from '../Typography';
 
 import { StyledPanel } from './styled';
 
-export type PanelProps = React.HTMLAttributes<HTMLElement> & MarginProps;
+export interface PanelAction extends Omit<ButtonProps, 'children'> {
+  text?: string;
+}
 
-export const RawPanel: React.FC<PanelProps> = props => (
-  <StyledPanel
-    marginBottom="medium"
-    {...props}
-    backgroundColor="white"
-    shadow="raised"
-    padding={{ mobile: 'medium', tablet: 'xxLarge' }}
-  />
-);
+export interface PanelProps extends React.HTMLAttributes<HTMLElement>, MarginProps {
+  header?: string;
+  action?: PanelAction;
+}
+
+export const RawPanel: React.FC<PanelProps> = memo(props => {
+  const {
+    action,
+    children,
+    header,
+    margin,
+    marginBottom,
+    marginHorizontal,
+    marginLeft,
+    marginRight,
+    marginTop,
+    marginVertical,
+  } = props;
+
+  const renderHeader = () => {
+    if (!header && !action) {
+      return null;
+    }
+
+    if (typeof header !== 'string') {
+      return null;
+    }
+
+    return (
+      <Flex justifyContent="space-between" flexDirection="row">
+        {header && (
+          <Flex.Item>
+            <H2>{header}</H2>
+          </Flex.Item>
+        )}
+        {action && (
+          <Flex.Item>
+            <Button {...action}>{action.text}</Button>
+          </Flex.Item>
+        )}
+      </Flex>
+    );
+  };
+
+  return (
+    <StyledPanel
+      backgroundColor="white"
+      shadow="raised"
+      padding={{ mobile: 'medium', tablet: 'xxLarge' }}
+      borderRadius="none"
+      margin={margin}
+      marginBottom={marginBottom || 'medium'}
+      marginHorizontal={marginHorizontal}
+      marginLeft={marginLeft}
+      marginRight={marginRight}
+      marginTop={marginTop}
+      marginVertical={marginVertical}
+    >
+      {renderHeader()}
+      {children}
+    </StyledPanel>
+  );
+});
 
 export const Panel: React.FC<PanelProps> = ({ className, style, ...props }) => <RawPanel {...props} />;
