b6f09c39b050350ddd65fdb55db9335ac870cf45
Chancellor Clark <chancellor.clark@bigcommerce.com>
2020-01-07 15:41:13 -0600

    feat(component): convert ProgressBar to FC (#315)

--- packages/big-design/src/components/ProgressBar/ProgressBar.tsx
+++ packages/big-design/src/components/ProgressBar/ProgressBar.tsx
@@ -6,18 +6,14 @@ export interface ProgressBarProps {
   percent?: number;
 }
 
-export class ProgressBar extends React.PureComponent<ProgressBarProps> {
-  render() {
-    const { percent } = this.props;
-
-    return typeof percent === 'number' ? (
-      <StyledProgressBar aria-valuemax={100} aria-valuemin={0} aria-valuenow={percent} role="progressbar">
-        <StyledProgressBarFiller percent={percent} />
-      </StyledProgressBar>
-    ) : (
-      <StyledProgressBar role="progressbar">
-        <StyledProgressBarFiller />
-      </StyledProgressBar>
-    );
-  }
-}
+export const ProgressBar: React.FC<ProgressBarProps> = ({ percent }) => {
+  return typeof percent === 'number' ? (
+    <StyledProgressBar aria-valuemax={100} aria-valuemin={0} aria-valuenow={percent} role="progressbar">
+      <StyledProgressBarFiller percent={percent} />
+    </StyledProgressBar>
+  ) : (
+    <StyledProgressBar role="progressbar">
+      <StyledProgressBarFiller />
+    </StyledProgressBar>
+  );
+};
