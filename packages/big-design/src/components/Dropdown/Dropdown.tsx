5e6e87fad2d0d7e63877004e70cb45e96840eeb5
Jorge Moya <jorge.moya@bigcommerce.com>
2020-02-05 11:47:48 -0600

    fix(): use Link

--- packages/big-design/src/components/Dropdown/Dropdown.tsx
+++ packages/big-design/src/components/Dropdown/Dropdown.tsx
@@ -5,6 +5,7 @@ import scrollIntoView from 'scroll-into-view-if-needed';
 import { uniqueId } from '../../utils';
 import { Flex } from '../Flex';
 import { FlexItem } from '../Flex/Item';
+import { Link } from '../Link';
 import { List } from '../List';
 import { ListGroupHeader } from '../List/GroupHeader';
 import { ListItem } from '../List/Item';
@@ -136,9 +137,9 @@ export class Dropdown<T extends any> extends React.PureComponent<DropdownProps<T
 
   private wrapInLink(option: DropdownLinkItem<T>, content: React.ReactChild) {
     return (
-      <a href={option.url} target={option.target}>
+      <Link href={option.url} target={option.target}>
         {content}
-      </a>
+      </Link>
     );
   }
 
