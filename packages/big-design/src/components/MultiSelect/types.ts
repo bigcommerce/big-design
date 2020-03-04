ee706dcbebe9d59f0ec8989340e68514236c38c6
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2020-03-04 11:37:26 -0600

    feat(component): allow null errors on Select and MultiSelect

--- packages/big-design/src/components/MultiSelect/types.ts
+++ packages/big-design/src/components/MultiSelect/types.ts
@@ -1,12 +1,13 @@
 import { Placement } from 'popper.js';
 import { RefObject } from 'react';
 
+import { InputProps } from '../Input';
 import { SelectAction, SelectOption } from '../Select';
 
 interface BaseSelect extends Omit<React.HTMLAttributes<HTMLInputElement>, 'children'> {
   action?: SelectAction;
   disabled?: boolean;
-  error?: string;
+  error?: InputProps['error'];
   filterable?: boolean;
   inputRef?: RefObject<HTMLInputElement> | React.Ref<HTMLInputElement>;
   label?: string;
