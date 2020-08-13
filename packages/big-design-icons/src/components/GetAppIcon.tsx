bb48f2d6aefa627f35137f6043b582268ac76ef4
William Kwon <50118040+bc-williamkwon@users.noreply.github.com>
2020-08-13 16:07:04 -0700

    feat(icons): add code, brush and get_app icons (#433)

---
+++ packages/big-design-icons/src/components/GetAppIcon.tsx
@@ -0,0 +1,38 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+import { useUniqueId } from '../utils';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useUniqueId('icon');
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+
+  return (
+    <svg
+      height={24}
+      viewBox="0 0 24 24"
+      width={24}
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      ref={svgRef}
+      aria-labelledby={titleId}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M16.59 9H15V4c0-.55-.45-1-1-1h-4c-.55 0-1 .45-1 1v5H7.41c-.89 0-1.34 1.08-.71 1.71l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.63-.63.19-1.71-.7-1.71zM5 19c0 .55.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1H6c-.55 0-1 .45-1 1z" />
+    </svg>
+  );
+};
+
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const GetAppIcon = memo(createStyledIcon(IconWithForwardedRef as React.FC<IconProps>));
+
+GetAppIcon.displayName = 'GetAppIcon';
