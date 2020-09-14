4241a3678d0da722b62567f01310ed0b1ff19cb8
William Kwon <william.kwon@bigcommerce.com>
2020-09-14 16:20:32 -0700

    fix(component): adjust spacing for ListGroupSeparator

--- packages/big-design/src/components/List/GroupSeparator/ListGroupSeparator.tsx
+++ packages/big-design/src/components/List/GroupSeparator/ListGroupSeparator.tsx
@@ -2,10 +2,12 @@ import React, { memo } from 'react';
 
 import { HR } from '../../Typography';
 
+import { StyledListItem } from './styled';
+
 export const ListGroupSeparator: React.FC<React.LiHTMLAttributes<HTMLLIElement>> = memo(() => (
-  <li tabIndex={-1} onMouseDown={preventFocus} aria-hidden={true}>
-    <HR />
-  </li>
+  <StyledListItem tabIndex={-1} onMouseDown={preventFocus} aria-hidden={true}>
+    <HR marginVertical="xSmall" />
+  </StyledListItem>
 ));
 
 function preventFocus(event: React.MouseEvent<HTMLLIElement, MouseEvent>) {
