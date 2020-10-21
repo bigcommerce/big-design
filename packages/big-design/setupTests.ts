4b6a28d67898f7784b746ff819643175d44780f5
Chancellor Clark <chancellor.clark@bigcommerce.com>
2020-10-21 10:10:26 -0500

    chore: remove unused uniqueId util in favor of hook

--- packages/big-design/setupTests.ts
+++ packages/big-design/setupTests.ts
@@ -1,34 +1,12 @@
 import { cleanup } from '@test/utils';
 import '@testing-library/jest-dom/extend-expect';
 
-import * as utils from './src/utils';
-
-/**
- * Because this util generates straightforward IDs which are saved in the snapshots,
- * changing the order of the unit tests or adding a new one in the middle of the file (Not at the end)
- * will break snapshots randomly, because generated IDs will no longer match the arbitrary order.
- * This mock allows us to artificially reset the counter after each `test` clause, so that
- * generated IDs within the same test are always the same, and other tests dont randomly break
- * when adding or removing tests.
- *
- * The additional `resetCounter` method does not exist in the original util, but we need it here
- * because jest.mock does not allow access to variables outside of its closure
- */
-jest.mock('./src/utils', () => {
-  let counter = 0;
-
-  return {
-    ...jest.requireActual<any>('./src/utils'),
-    warning: jest.fn(),
-    resetCounter: () => (counter = 0),
-    uniqueId: (context: string) => {
-      return `${context}${counter++}`;
-    },
-  };
-});
+jest.mock('./src/utils', () => ({
+  ...jest.requireActual<any>('./src/utils'),
+  warning: jest.fn(),
+}));
 
 afterEach(() => {
-  (utils as any).resetCounter();
   jest.clearAllMocks();
 
   // https://github.com/testing-library/react-testing-library/pull/519
