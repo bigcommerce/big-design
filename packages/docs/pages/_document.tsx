b2052279a8ea79a3f449ce4dd770d69d7b1b5c39
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2020-12-09 09:47:36 -0600

    fix(docs): add missing Html component to document

--- packages/docs/pages/_document.tsx
+++ packages/docs/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -35,13 +35,13 @@ export default class AppDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         {this.renderGTM()}
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 
