4962086de0a07b2cd422fbaa6615cf9a155fbe66
Kevin Chu <kevin.chu@bigcommerce.com>
2021-01-29 10:17:12 -0800

    feat(icons): add cloud upload icon

---
+++ packages/big-design-icons/src/components/CloudUploadIcon.tsx
@@ -0,0 +1,38 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+import { useUniqueId } from '../utils';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useUniqueId('icon');
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+
+  return (
+    <svg
+      height={24}
+      viewBox="0 0 24 24"
+      width={24}
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      ref={svgRef}
+      aria-labelledby={titleId}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M19.35 10.04A7.49 7.49 0 0012 4C9.11 4 6.6 5.64 5.35 8.04A5.994 5.994 0 000 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l4.65-4.65c.2-.2.51-.2.71 0L17 13h-3z" />
+    </svg>
+  );
+};
+
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const CloudUploadIcon = memo(createStyledIcon(IconWithForwardedRef as React.FC<IconProps>));
+
+CloudUploadIcon.displayName = 'CloudUploadIcon';
