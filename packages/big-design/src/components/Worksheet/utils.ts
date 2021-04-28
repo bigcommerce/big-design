63c2313fa14945c0ba7f1ae748e841bcf0b60add
Jorge Moya <jorge.moya@bigcommerce.com>
2021-04-28 14:46:48 -0500

    refactor: return array of items when onChange or onError

--- packages/big-design/src/components/Worksheet/utils.ts
+++ packages/big-design/src/components/Worksheet/utils.ts
@@ -1,6 +1,6 @@
-import { Cell } from './types';
+import { Cell, WorksheetError, WorksheetItem } from './types';
 
-export const mergeCells = <T extends unknown>(oldCells: Cell<T>[], newCells: Cell<T>[]) =>
+export const mergeCells = <T extends WorksheetItem>(oldCells: Cell<T>[], newCells: Cell<T>[]) =>
   newCells.reduce(
     (accum, newCell) => {
       const index = oldCells.findIndex(
@@ -18,8 +18,33 @@ export const mergeCells = <T extends unknown>(oldCells: Cell<T>[], newCells: Cel
     [...oldCells], // Note: returns a new array every time
   );
 
-export const deleteCells = <T extends unknown>(oldCells: Cell<T>[], newCells: Cell<T>[]) =>
+export const deleteCells = <T extends WorksheetItem>(oldCells: Cell<T>[], newCells: Cell<T>[]) =>
   oldCells.filter(
     (oldCell) =>
       !newCells.find((newCell) => newCell.columnIndex === oldCell.columnIndex && newCell.rowIndex === oldCell.rowIndex),
   );
+
+export const editedRows = <T extends WorksheetItem>(editedCells: Cell<T>[], rows: T[]) =>
+  editedCells.reduce<T[]>((accum, editedCell) => {
+    const row = rows[editedCell.rowIndex];
+
+    if (accum.find((editedRow) => editedRow === row)) {
+      return accum;
+    } else {
+      return [...accum, row];
+    }
+  }, []);
+
+export const invalidRows = <T extends WorksheetItem>(invalidCells: Cell<T>[], rows: T[]) =>
+  invalidCells.reduce<WorksheetError<T>[]>((accum, invalidCell) => {
+    const row = rows[invalidCell.rowIndex];
+    const rowIndex = accum.findIndex((invalidRow) => invalidRow.item === row);
+
+    if (rowIndex >= 0) {
+      accum[rowIndex].errors = [...accum[rowIndex].errors, invalidCell.hash];
+
+      return accum;
+    } else {
+      return [...accum, { item: row, errors: [invalidCell['hash']] }];
+    }
+  }, []);
