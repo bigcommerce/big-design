048b43a94215fde12d7a05c63251c69a06710a2c
Jorge Moya <jorge.moya@bigcommerce.com>
2021-05-14 14:44:45 -0500

    feat(component): allow formatting of columns

--- packages/big-design/src/components/Worksheet/editors/ModalEditor/ModalEditor.tsx
+++ packages/big-design/src/components/Worksheet/editors/ModalEditor/ModalEditor.tsx
@@ -4,15 +4,16 @@ import { typedMemo } from '../../../../utils';
 import { Flex, FlexItem } from '../../../Flex';
 import { Small } from '../../../Typography';
 import { useStore } from '../../hooks';
-import { Cell, WorksheetItem } from '../../types';
+import { Cell, WorksheetItem, WorksheetModalColumn } from '../../types';
 
 import { StyledButton } from './styled';
 
 export interface ModalEditorProps<Item> {
   cell: Cell<Item>;
+  formatting?: WorksheetModalColumn<Item>['formatting'];
 }
 
-const InternalModalEditor = <T extends WorksheetItem>({ cell }: ModalEditorProps<T>) => {
+const InternalModalEditor = <T extends WorksheetItem>({ cell, formatting }: ModalEditorProps<T>) => {
   const setOpenModal = useStore((state) => state.setOpenModal);
   const { hash, value } = cell;
 
@@ -23,7 +24,7 @@ const InternalModalEditor = <T extends WorksheetItem>({ cell }: ModalEditorProps
   return (
     <Flex justifyContent="space-between" alignItems="center">
       <FlexItem paddingRight="small">
-        <Small>{String(value)}</Small>
+        <Small>{formatting ? formatting(value) : `${value}`}</Small>
       </FlexItem>
       <StyledButton onClick={handleClick} variant="subtle">
         Edit
