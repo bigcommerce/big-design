2350481ef113f317b5cd9db74a8337e63866bfcb
Stanislav Holts <stanislav.holts@bigcommerce.com>
2021-07-07 18:33:17 +0300

    feat(component): add ButtonGroup component (#556)
    * feat(component): add ButtonGroup component
    
    * fix: comments
    
    * fix: grammatic

---
+++ packages/big-design/src/components/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,154 @@
+import { MoreHorizIcon } from '@bigcommerce/big-design-icons';
+import React, { createRef, HTMLAttributes, memo, useCallback, useEffect, useMemo, useState } from 'react';
+
+import { useWindowResizeListener } from '../../hooks';
+import { MarginProps } from '../../mixins';
+import { ButtonProps } from '../Button';
+import { Dropdown } from '../Dropdown';
+import { Flex } from '../Flex';
+
+import { StyledButton, StyledFlexItem } from './styled';
+
+export interface ButtonGroupAction extends Omit<ButtonProps, 'children' | 'iconOnly' | 'iconRight' | 'iconLeft'> {
+  text: string;
+  icon?: React.ReactElement;
+}
+
+export interface ButtonGroupProps extends HTMLAttributes<HTMLDivElement>, MarginProps {
+  actions: ButtonGroupAction[];
+}
+
+const excludeIconProps = ({
+  iconOnly,
+  iconRight,
+  iconLeft,
+  ...actionProps
+}: ButtonProps & Pick<ButtonGroupAction, 'text'>): ButtonGroupAction => actionProps;
+
+export const ButtonGroup: React.FC<ButtonGroupProps> = memo(({ actions, ...wrapperProps }) => {
+  const parentRef = createRef<HTMLDivElement>();
+  const dropdownRef = createRef<HTMLDivElement>();
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [actionsState, setActionsState] = useState(
+    actions.map((action) => ({ isVisible: true, action: excludeIconProps(action), ref: createRef<HTMLDivElement>() })),
+  );
+
+  const hideOverflowedActions = useCallback(() => {
+    const parentWidth = parentRef.current?.offsetWidth;
+    const dropdownWidth = dropdownRef.current?.offsetWidth;
+
+    if (!parentWidth || !dropdownWidth) {
+      return;
+    }
+
+    let remainingWidth = parentWidth;
+
+    const nextState = actionsState.map((stateObj) => {
+      const actionWidth = stateObj.ref.current?.offsetWidth;
+
+      if (!actionWidth) {
+        return stateObj;
+      }
+
+      if (stateObj.action.actionType === 'destructive') {
+        return { ...stateObj, isVisible: false };
+      }
+
+      if (remainingWidth - actionWidth > dropdownWidth) {
+        remainingWidth = remainingWidth - actionWidth;
+
+        return { ...stateObj, isVisible: true };
+      }
+
+      return { ...stateObj, isVisible: false };
+    });
+
+    const visibleActions = actionsState.filter(({ isVisible }) => isVisible);
+    const nextVisibleActions = nextState.filter(({ isVisible }) => isVisible);
+
+    if (visibleActions.length !== nextVisibleActions.length) {
+      setActionsState(nextState);
+    }
+  }, [actionsState, dropdownRef, parentRef]);
+
+  const renderedDropdown = useMemo(
+    () => (
+      <StyledFlexItem data-testid="buttongroup-dropdown" isVisible={isMenuVisible} ref={dropdownRef} role="listitem">
+        <Dropdown
+          items={actionsState
+            .filter(({ isVisible }) => !isVisible)
+            .map(({ action, ref }) => ({
+              actionType: action.actionType,
+              content: action.text,
+              disabled: action.disabled,
+              onItemClick: () => {
+                if (ref.current) {
+                  ref.current.getElementsByTagName('button')[0].click();
+                }
+              },
+              hash: action.text.toLowerCase(),
+              icon: action.icon,
+            }))}
+          toggle={
+            <StyledButton
+              borderRadius={actionsState.every(({ isVisible }) => !isVisible)}
+              iconOnly={<MoreHorizIcon title="more" />}
+              variant="secondary"
+            />
+          }
+          placement="bottom-end"
+        />
+      </StyledFlexItem>
+    ),
+    [actionsState, dropdownRef, isMenuVisible],
+  );
+
+  const renderedActions = useMemo(
+    () =>
+      [...actionsState]
+        .reverse()
+        .sort(({ isVisible }) => (isVisible ? -1 : 1))
+        .map(({ action, isVisible, ref }, key) => {
+          const { text, icon, ...buttonProps } = action;
+
+          return (
+            <StyledFlexItem data-testid="buttongroup-item" key={key} isVisible={isVisible} ref={ref} role="listitem">
+              <StyledButton {...buttonProps} variant="secondary">
+                {text}
+              </StyledButton>
+            </StyledFlexItem>
+          );
+        }),
+    [actionsState],
+  );
+
+  useEffect(() => {
+    const nextIsMenuVisible = actionsState.some(({ isVisible }) => !isVisible);
+
+    if (nextIsMenuVisible !== isMenuVisible) {
+      setIsMenuVisible(nextIsMenuVisible);
+    }
+  }, [actionsState, isMenuVisible]);
+
+  useEffect(() => {
+    hideOverflowedActions();
+  }, [actions, parentRef, hideOverflowedActions]);
+
+  useWindowResizeListener(() => {
+    hideOverflowedActions();
+  });
+
+  return actions.length > 0 ? (
+    <Flex
+      {...wrapperProps}
+      data-testid="buttongroup-wrapper"
+      flexDirection="row"
+      flexWrap="nowrap"
+      ref={parentRef}
+      role="list"
+    >
+      {renderedActions}
+      {renderedDropdown}
+    </Flex>
+  ) : null;
+});
---
+++ packages/big-design/src/components/ButtonGroup/styled.tsx
@@ -0,0 +1,57 @@
+import { theme as defaultTheme } from '@bigcommerce/big-design-theme';
+import styled, { css } from 'styled-components';
+
+import { StyleableButton } from '../Button/private';
+import { FlexItem } from '../Flex';
+
+interface StyledButtonProps {
+  borderRadius?: boolean;
+}
+
+interface StyledFlexItemProps {
+  isVisible: boolean;
+}
+
+export const StyledButton = styled(StyleableButton)<StyledButtonProps>`
+  ${({ borderRadius, theme }) =>
+    borderRadius
+      ? css`
+          border-radius: ${theme.borderRadius.normal};
+        `
+      : css`
+          border-radius: ${theme.borderRadius.none};
+        `}
+
+  &:focus {
+    z-index: 1;
+  }
+`;
+
+export const StyledFlexItem = styled(FlexItem)<StyledFlexItemProps>`
+  margin-right: -1px;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+
+  &:first-of-type ${StyledButton} {
+    border-bottom-left-radius: ${({ theme }) => theme.borderRadius.normal};
+    border-top-left-radius: ${({ theme }) => theme.borderRadius.normal};
+  }
+
+  &:nth-last-of-type(-n + 2) ${StyledButton} {
+    border-bottom-right-radius: ${({ theme }) => theme.borderRadius.normal};
+    border-top-right-radius: ${({ theme }) => theme.borderRadius.normal};
+  }
+
+  ${({ isVisible }) =>
+    !isVisible &&
+    css`
+      position: absolute;
+      visibility: hidden;
+      z-index: ${({ theme }) => -theme.zIndex.tooltip};
+    `}
+`;
+
+StyledButton.defaultProps = { theme: defaultTheme };
+StyledFlexItem.defaultProps = { theme: defaultTheme };
