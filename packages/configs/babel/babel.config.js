2fb746a5f9fe96fe9c51ef587c8d1ebe1b0ff685
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2021-09-20 10:14:17 -0500

    chore(configs): update babel browserslist

--- packages/configs/babel/babel.config.js
+++ packages/configs/babel/babel.config.js
@@ -2,6 +2,16 @@ const BABEL_ENV = process.env.BABEL_ENV;
 
 const defaultPlugins = [['babel-plugin-styled-components', { pure: true }]];
 
+const browsers = [
+  'last 2 Chrome major versions',
+  'last 2 Firefox major versions',
+  'last 2 Safari major versions',
+  'last 2 Edge major versions',
+  'last 2 Android major versions',
+  'last 2 ChromeAndroid major versions',
+  'last 2 iOS major versions',
+];
+
 module.exports = function (api) {
   api.cache(true);
 
@@ -11,7 +21,7 @@ module.exports = function (api) {
         '@babel/preset-env',
         {
           modules: ['cjs', 'test'].includes(BABEL_ENV) ? 'commonjs' : false,
-          targets: BABEL_ENV === 'test' ? { node: 'current' } : { browsers: 'defaults' },
+          targets: BABEL_ENV === 'test' ? { node: 'current' } : { browsers },
         },
       ],
       '@babel/preset-typescript',
