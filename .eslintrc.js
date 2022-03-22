7952c53793e3765bb4a1029efc94d1b813b51ec6
Chancellor Clark <chancellor.clark@bigcommerce.com>
2022-03-22 10:05:44 -0500

    chore: update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
+require('@bigcommerce/eslint-config/patch');
+
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['@bigcommerce/eslint-config'],
+  parserOptions: {
+    project: [
+      './packages/big-design/tsconfig.json',
+      './packages/big-design-icons/tsconfig.json',
+      './packages/big-design-theme/tsconfig.json',
+      './packages/docs/tsconfig.json',
+      './packages/examples/tsconfig.json',
+    ],
+    tsconfigRootDir: __dirname,
+  },
   settings: {
-    react: {
-      version: 'detect',
-    },
     'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
@@ -24,63 +21,4 @@ module.exports = {
       },
     },
   },
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
-    '@typescript-eslint/no-use-before-define': 'off',
-    'no-console': 'warn',
-    'react/display-name': 'off',
-    'react/prop-types': 'off',
-    'react/no-unescaped-entities': [
-      'error',
-      {
-        forbid: [
-          { char: '>', alternatives: ['&gt;'] },
-          { char: '}', alternatives: ['&#125;'] },
-        ],
-      },
-    ],
-    'newline-before-return': 'error',
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-      },
-    ],
-    'import/named': 'off',
-    // Remove this when https://github.com/benmosher/eslint-plugin-import/pull/1528 gets released
-    'import/default': 'off',
-    'import/order': [
-      'error',
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
-        groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: '**/*.js',
-      env: {
-        node: true,
-        es6: true,
-      },
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
-    {
-      files: 'packages/big-design-icons/**/*.tsx',
-      rules: {
-        'react/jsx-sort-props': 'warn',
-      },
-    },
-  ],
 };
