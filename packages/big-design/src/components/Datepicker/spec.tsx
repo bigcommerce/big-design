e4e506bd58812dcbb7c0bad68c59392bd7baceac
Maria Navarro <maria.navarro@bigcommerce.com>
2022-05-24 11:56:28 -0500

    chore(deps): add type in datepicker jest file

--- packages/big-design/src/components/Datepicker/spec.tsx
+++ packages/big-design/src/components/Datepicker/spec.tsx
@@ -10,11 +10,28 @@ import { FormGroup } from '..';
 
 import { Datepicker } from './index';
 
+type Placements =
+  | 'auto'
+  | 'auto-end'
+  | 'auto-start'
+  | 'bottom'
+  | 'bottom-end'
+  | 'bottom-start'
+  | 'left'
+  | 'left-end'
+  | 'left-start'
+  | 'right'
+  | 'right-end'
+  | 'right-start'
+  | 'top'
+  | 'top-end'
+  | 'top-start';
+
 jest.mock(
   'popper.js',
   () =>
     class Popper {
-      static placements = [
+      static placements: Placements[] = [
         'auto',
         'auto-end',
         'auto-start',
