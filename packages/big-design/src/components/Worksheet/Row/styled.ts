1c0fe8ab7afadcfc36c33099d2d3d0c424b49da9
bc-apostoliuk <andrii.postoliuk@bigcommerce.com>
2023-01-25 14:00:41 +0200

    feat(component): add sub-rows indicators for worksheet

--- packages/big-design/src/components/Worksheet/Row/styled.ts
+++ packages/big-design/src/components/Worksheet/Row/styled.ts
@@ -1,10 +1,8 @@
 import { theme as defaultTheme } from '@bigcommerce/big-design-theme';
 import styled from 'styled-components';
 
-export const StyledTableRow = styled.tr<{ isChild: boolean; isExpanded: boolean }>`
+export const StyledTableRow = styled.tr<{ isExpanded: boolean }>`
   display: ${({ isExpanded }) => (isExpanded ? 'table-row' : 'none')};
-  background-color: ${({ theme, isChild }) =>
-    isChild ? theme.colors.secondary10 : theme.colors.white};
 `;
 
 StyledTableRow.defaultProps = { theme: defaultTheme };
