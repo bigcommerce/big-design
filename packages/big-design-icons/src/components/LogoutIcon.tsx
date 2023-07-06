1902f22a051ffad3eb99524017a4b2f3959957c1
Robert David Banbanaste <106188209+BC-Banbanaste@users.noreply.github.com>
2023-07-06 11:50:46 -0500

    CPX-160 add logout icon (#1256)
    * chore: downgrade inquirer and inquirer-autocomplete-prompt to cjs
    
    * chore: add ignore packages to dependabot
    
    * chore: formating
    
    * feat: add logout icon

---
+++ packages/big-design-icons/src/components/LogoutIcon.tsx
@@ -0,0 +1,40 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo, useId } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useId();
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+  const ariaHidden = titleId ? undefined : true;
+
+  return (
+    <svg
+      aria-hidden={ariaHidden}
+      aria-labelledby={titleId}
+      fill="currentColor"
+      height={24}
+      ref={svgRef}
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      width={24}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M5 5h6c.55 0 1-.45 1-1s-.45-1-1-1H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h6c.55 0 1-.45 1-1s-.45-1-1-1H5V5z" />
+      <path d="M20.65 11.65l-2.79-2.79a.501.501 0 00-.86.35V11h-7c-.55 0-1 .45-1 1s.45 1 1 1h7v1.79c0 .45.54.67.85.35l2.79-2.79c.2-.19.2-.51.01-.7z" />
+    </svg>
+  );
+};
+
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const LogoutIcon = memo(createStyledIcon(IconWithForwardedRef));
+
+LogoutIcon.displayName = 'LogoutIcon';
