bc4e6d128e07b61b30b679b608e9faac8ab4b3f4
bc-apostoliuk <andrii.postoliuk@bigcommerce.com>
2023-09-28 12:41:58 +0300

    feat(icons): add Draft icon

---
+++ packages/big-design-icons/src/components/DraftIcon.tsx
@@ -0,0 +1,38 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo, useId } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useId();
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+  const ariaHidden = titleId ? undefined : true;
+
+  return (
+    <svg
+      aria-hidden={ariaHidden}
+      aria-labelledby={titleId}
+      fill="currentColor"
+      height={48}
+      ref={svgRef}
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 -960 960 960"
+      width={48}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M220-80q-24 0-42-18t-18-42v-680q0-24 18-42t42-18h361l219 219v521q0 24-18 42t-42 18H220zm331-554v-186H220v680h520v-494H551zM220-820v186-186 680-680z" />
+    </svg>
+  );
+};
+
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const DraftIcon = memo(createStyledIcon(IconWithForwardedRef));
+
+DraftIcon.displayName = 'DraftIcon';
--- packages/big-design-icons/src/components/index.ts
+++ packages/big-design-icons/src/components/index.ts
@@ -17,6 +17,7 @@ export * from './CloudUploadIcon';
 export * from './CodeIcon';
 export * from './ContentCopyIcon';
 export * from './DeleteIcon';
+export * from './DraftIcon';
 export * from './DragIndicatorIcon';
 export * from './EditIcon';
 export * from './ErrorIcon';
