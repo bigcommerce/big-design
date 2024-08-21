250dcd1d380ae18eee2fa8650f1e71f02c99580a
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-08-21 11:00:32 -0400

    chore: adjust commitlint to account for dependabot prs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
+/** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-max-line-length': [2, 'always', 180],
     'scope-enum': [
       2,
       'always',
