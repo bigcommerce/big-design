1246428064daa5e46f9c910921e7f84fdcd4f3cf
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-08-23 13:01:27 -0400

    feat(patterns): implement the Page component
    Co-authored-by: Daniel Almaguer <daniel.almaguer@bigcommerce.com>

---
+++ packages/big-design-patterns/src/components/Page/spec.tsx
@@ -0,0 +1,99 @@
+import { render, screen, within } from '@testing-library/react';
+import React from 'react';
+
+import { warning } from '../../utils';
+import { Header } from '../Header';
+
+import { Page } from './Page';
+
+test('renders children', () => {
+  render(<Page>Page content</Page>);
+
+  expect(screen.getByText('Page content')).toBeInTheDocument();
+});
+
+test('renders with header', () => {
+  render(<Page header={<Header title="Page Title" />}>Page content</Page>);
+
+  expect(screen.getByRole('heading', { name: 'Page Title' })).toBeInTheDocument();
+});
+
+test('warns when header is not a Header component', () => {
+  render(<Page header={<div>Not a Header</div>}>Page content</Page>);
+
+  expect(warning).toHaveBeenCalledWith('A `Header` component is required for the `header` prop.');
+});
+
+test('renders with message', () => {
+  render(<Page message={{ messages: [{ text: 'Test message' }] }}>Page content</Page>);
+
+  const message = screen.getByRole('alert');
+
+  expect(within(message).getByText('Test message')).toBeInTheDocument();
+});
+
+test('does not render message when messages array is empty', () => {
+  render(<Page message={{ messages: [] }}>Page content</Page>);
+
+  expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+});
+
+test('renders with background', () => {
+  const backgroundProps = {
+    src: 'test-image.jpg',
+    backgroundSize: 'cover',
+    backgroundPosition: 'left',
+    backgroundRepeat: 'no-repeat',
+  };
+
+  const { container } = render(<Page background={backgroundProps}>Page content</Page>);
+  const firstChild = container.firstElementChild;
+
+  if (!firstChild) {
+    throw new Error();
+  }
+
+  expect(firstChild).toHaveStyle(`
+      background-image: url(test-image.jpg);
+      background-size: cover;
+      background-position: left;
+      background-repeat: no-repeat;
+    `);
+});
+
+test('renders with default background styles', () => {
+  const backgroundProps = {
+    src: 'test-image.jpg',
+  };
+
+  const { container } = render(<Page background={backgroundProps}>Page content</Page>);
+  const firstChild = container.firstElementChild;
+
+  if (!firstChild) {
+    throw new Error();
+  }
+
+  expect(firstChild).toHaveStyle(`
+      background-image: url(test-image.jpg);
+      background-size: contain;
+      background-position: top right;
+      background-repeat: no-repeat;
+    `);
+});
+
+test('renders without background by default', () => {
+  render(<Page>Page content</Page>);
+
+  const { container } = render(<Page>Page content</Page>);
+  const firstChild = container.firstElementChild;
+
+  if (!firstChild) {
+    throw new Error();
+  }
+
+  expect(firstChild).not.toHaveStyle('background-size: contain');
+  expect(firstChild).toHaveStyle(`
+    background-color: #F6F7FC;
+    min-height: 100dvh;
+  `);
+});
