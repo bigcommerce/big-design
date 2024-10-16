015bf000558dcf9e99bc8f35fa2e6a3eac29db2a
David Linke <davelinke@users.noreply.github.com>
2024-10-16 17:59:24 -0400

    feat(component): fix component structure and tests

--- packages/big-design/src/components/FeatureTag/spec.tsx
+++ packages/big-design/src/components/FeatureTag/spec.tsx
@@ -1,3 +1,5 @@
+import { AutoAwesomeIcon } from '@bigcommerce/big-design-icons';
+import { theme } from '@bigcommerce/big-design-theme';
 import { render } from '@testing-library/react';
 import React from 'react';
 
@@ -5,14 +7,32 @@ import 'jest-styled-components';
 
 import { FeatureTag } from './FeatureTag';
 
-test('render link', () => {
-  const { container } = render(<FeatureTag href="#" label="Feature Tag" />);
+test('render feature tag', () => {
+  const { container } = render(<FeatureTag label="Feature Tag" />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('renders with external icon', () => {
-  const { container } = render(<FeatureTag href="#" label="Feature Tag" target="_blank" />);
+test('renders with icon', () => {
+  const { container } = render(<FeatureTag icon={<AutoAwesomeIcon />} label="Feature Tag" />);
 
   expect(container.firstChild).toMatchSnapshot();
+});
+
+test('renders as active', () => {
+  const { container } = render(
+    <FeatureTag icon={<AutoAwesomeIcon />} isActive={true} label="Feature Tag" />,
+  );
+
+  expect(container.firstChild).toMatchSnapshot();
+  expect(container.firstChild).toHaveStyle(`background-color: ${theme.colors.primary10}`);
+  expect(container.firstChild).toHaveStyle(`color: ${theme.colors.primary50}`);
+  expect(container.firstChild).toHaveStyle(`fill: ${theme.colors.primary}`);
+});
+
+test("doesn't render if label prop is absent", () => {
+  // @ts-expect-error ignoring since label prop is required
+  const { container } = render(<FeatureTag />);
+
+  expect(container.firstChild).toBeNull();
 });
