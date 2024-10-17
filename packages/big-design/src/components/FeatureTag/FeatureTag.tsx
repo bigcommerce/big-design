27409ecd99a7244a5b5708fe642d4a49b10cc95c
David Linke <davelinke@users.noreply.github.com>
2024-10-17 09:49:17 -0400

    feat(component): add html anchor props

--- packages/big-design/src/components/FeatureTag/FeatureTag.tsx
+++ packages/big-design/src/components/FeatureTag/FeatureTag.tsx
@@ -1,17 +1,28 @@
 import { IconProps } from '@bigcommerce/big-design-icons';
-import React, { FunctionComponent, HTMLAttributes, ReactElement } from 'react';
+import React, { AnchorHTMLAttributes, FunctionComponent, ReactElement } from 'react';
 
 import { StyledFeatureTag, StyledFeatureTagIcon, StyledFeatureTagLabel } from './styled';
 
-export interface FeatureTagProps extends HTMLAttributes<HTMLAnchorElement> {
+export interface FeatureTagProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   icon?: ReactElement<IconProps>;
   isActive?: boolean;
   label: string;
 }
 
-export const FeatureTag: FunctionComponent<FeatureTagProps> = ({ label, icon, isActive }) => {
+export const FeatureTag: FunctionComponent<FeatureTagProps> = ({
+  label,
+  icon,
+  isActive,
+  href,
+  target,
+}) => {
   return label ? (
-    <StyledFeatureTag className={isActive ? 'active' : ''} title={label}>
+    <StyledFeatureTag
+      className={isActive ? 'active' : ''}
+      href={href}
+      target={target}
+      title={label}
+    >
       {icon ? <StyledFeatureTagIcon>{icon}</StyledFeatureTagIcon> : null}
       <StyledFeatureTagLabel>{label}</StyledFeatureTagLabel>
     </StyledFeatureTag>
