40de9acc2422b423a5d35e438e6de14b53b0fb80
David Linke <davelinke@users.noreply.github.com>
2024-12-13 15:05:40 -0500

    feat(component): remove html anchor functionality from FeatureTag

--- packages/big-design/src/components/FeatureTag/spec.tsx
+++ packages/big-design/src/components/FeatureTag/spec.tsx
@@ -1,5 +1,4 @@
 import { AutoAwesomeIcon } from '@bigcommerce/big-design-icons';
-import { theme } from '@bigcommerce/big-design-theme';
 import { render } from '@testing-library/react';
 import React from 'react';
 
@@ -17,17 +16,6 @@ test('renders with icon', () => {
   const { container } = render(<FeatureTag icon={<AutoAwesomeIcon />} label="Feature Tag" />);
 
   expect(container.firstChild).toMatchSnapshot();
-});
-
-test('renders as active', () => {
-  const { container } = render(
-    <FeatureTag icon={<AutoAwesomeIcon />} isActive={true} label="Feature Tag" />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-  expect(container.firstChild).toHaveStyle(`background-color: ${theme.colors.primary10}`);
-  expect(container.firstChild).toHaveStyle(`color: ${theme.colors.primary50}`);
-  expect(container.firstChild).toHaveStyle(`fill: ${theme.colors.primary}`);
 });
 
 test("doesn't render if label prop is absent", () => {
