5e0589f8c48a06287f0d0e2e5e87af54229fdcfd
David Linke <davelinke@users.noreply.github.com>
2025-01-29 10:30:49 -0500

    feat(component): implement FeatureTag component (#1567)
    * feat(component): create FeatureTag base components
    
    * feat(component): fix component structure and tests
    
    * feat(component): add html anchor props
    
    * feat(component): fix test version mismatch
    
    * feat(component): remove html anchor functionality from FeatureTag
    
    * feat: add FeatureSet component
    
    * feat(component): lint FeatureSet component
    
    * feat(component): refresh FeatureSet snapshot
    
    * feat(component): add FeatureSet documentation
    
    * feat(component): add changeset for FeatureSet
    
    * refactor(component): reorg and cleanup of styles
    
    ---------
    
    Co-authored-by: Chancellor Clark <chancellor.clark@bigcommerce.com>

---
+++ packages/docs/pages/feature-set.tsx
@@ -0,0 +1,74 @@
+import { FeatureSet, H1, Panel, Text } from '@bigcommerce/big-design';
+import { AutoAwesomeIcon } from '@bigcommerce/big-design-icons';
+import React from 'react';
+
+import { Code, CodePreview, ContentRoutingTabs, GuidelinesTable } from '../components';
+import { FeatureSetPropTable, FeatureTagPropTable } from '../PropTables/FeatureSetPropTable';
+
+const FeatureSetPage = () => {
+  return (
+    <>
+      <H1>Feature Set</H1>
+
+      <Panel header="Overview" headerId="overview">
+        <Text>
+          <Code primary>Feature sets</Code> are uset to represent a summarized version of
+          application features.
+        </Text>
+        <Text>
+          They are to be used within installations and other similar pages where a quick glance of
+          relevant characteristics is needed.
+        </Text>
+      </Panel>
+
+      <Panel header="Implementation" headerId="implementation">
+        <CodePreview>
+          {/* jsx-to-string:start */}
+          {function Example() {
+            const tags = [
+              { label: 'Feature 1', icon: <AutoAwesomeIcon /> },
+              { label: 'Feature 2', icon: <AutoAwesomeIcon /> },
+              { label: 'Feature 3' },
+            ];
+
+            return <FeatureSet tags={tags} />;
+          }}
+          {/* jsx-to-string:end */}
+        </CodePreview>
+      </Panel>
+
+      <Panel header="Props" headerId="props">
+        <ContentRoutingTabs
+          id="props"
+          routes={[
+            {
+              id: 'feature-set',
+              title: 'FeatureSet',
+              render: () => <FeatureSetPropTable />,
+            },
+            {
+              id: 'feature-tag',
+              title: 'FeatureTag',
+              render: () => <FeatureTagPropTable id="feature-tag-prop-table" />,
+            },
+          ]}
+        />
+      </Panel>
+
+      <Panel header="Do's and Don'ts" headerId="guidelines">
+        <GuidelinesTable
+          discouraged={[
+            'Do not use feature tags to convey application status.',
+            'Do not use as a call to action element.',
+          ]}
+          recommended={[
+            'Use short feature descriptions.',
+            'Use relevant icons associated to the feature description.',
+          ]}
+        />
+      </Panel>
+    </>
+  );
+};
+
+export default FeatureSetPage;
