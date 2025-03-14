0c11db02b7c768de6a6f6ad4141e97e20fdf9323
David Linke <davelinke@users.noreply.github.com>
2025-03-14 11:03:47 -0400

    feat(icons): add LightbulbIcon (#1656)
    * feat(icons): add LightbulbIcon
    
    * feat(icons): add changeset for lightbulb icon

---
+++ packages/big-design-icons/src/components/LightbulbIcon.tsx
@@ -0,0 +1,37 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo, useId } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useId();
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+  const ariaHidden = titleId ? undefined : true;
+
+  return (
+    <svg
+      aria-hidden={ariaHidden}
+      aria-labelledby={titleId}
+      fill="currentColor"
+      height={24}
+      ref={svgRef}
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      width={24}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2M9 19h6c.55 0 1-.45 1-1s-.45-1-1-1H9c-.55 0-1 .45-1 1s.45 1 1 1M12 2C7.86 2 4.5 5.36 4.5 9.5c0 3.82 2.66 5.86 3.77 6.5h7.46c1.11-.64 3.77-2.68 3.77-6.5C19.5 5.36 16.14 2 12 2" />
+    </svg>
+  );
+};
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const LightbulbIcon = memo(createStyledIcon(IconWithForwardedRef));
+LightbulbIcon.displayName = 'LightbulbIcon';
