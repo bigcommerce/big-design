acda7cc3184d85a52b695466f996fed22eeec64e
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-05-05 10:51:04 +1000

    refactor(component): add content routing tabs to Pill Tabs docs to make room for extra examples

--- packages/docs/pages/pill-tabs.tsx
+++ packages/docs/pages/pill-tabs.tsx
@@ -25,83 +25,98 @@ const PillTabsPage = () => {
       </Panel>
 
       <Panel header="Implementation" headerId="implementation">
-        <CodePreview>
-          {/* jsx-to-string:start */}
-          {function Example() {
-            const [activePills, setActivePills] = useState<string[]>([]);
-            const Card: React.FC<{ name: string; description: string }> = ({
-              name,
-              description,
-            }) => (
-              <Flex
-                border="box"
-                borderRadius="normal"
-                flexDirection="column"
-                margin="xxSmall"
-                padding="medium"
-              >
-                <FlexItem marginBottom="xxSmall">
-                  <Text bold>{name}</Text>
-                </FlexItem>
-                <FlexItem flexGrow={1}>
-                  <Text>{description}</Text>
-                </FlexItem>
-                <FlexItem>
-                  <Link href="#">Install</Link>
-                </FlexItem>
-              </Flex>
-            );
-            const items = [
-              { title: 'Shipping', id: 'shipping' },
-              { title: 'Orders', id: 'orders' },
-            ];
-            const onPillClick = (pillId: string) => {
-              const isPillActive = !activePills.includes(pillId);
-              const updatedPills = isPillActive
-                ? [...activePills, pillId]
-                : activePills.filter((activePillId) => activePillId !== pillId);
+        <ContentRoutingTabs
+          id="implementation"
+          routes={[
+            {
+              id: 'basic',
+              title: 'Basic',
+              render: () => (
+                <CodePreview>
+                  {/* jsx-to-string:start */}
+                  {function Example() {
+                    const [activePills, setActivePills] = useState<string[]>([]);
+                    const Card: React.FC<{ name: string; description: string }> = ({
+                      name,
+                      description,
+                    }) => (
+                      <Flex
+                        border="box"
+                        borderRadius="normal"
+                        flexDirection="column"
+                        margin="xxSmall"
+                        padding="medium"
+                      >
+                        <FlexItem marginBottom="xxSmall">
+                          <Text bold>{name}</Text>
+                        </FlexItem>
+                        <FlexItem flexGrow={1}>
+                          <Text>{description}</Text>
+                        </FlexItem>
+                        <FlexItem>
+                          <Link href="#">Install</Link>
+                        </FlexItem>
+                      </Flex>
+                    );
+                    const items = [
+                      { title: 'Shipping', id: 'shipping' },
+                      { title: 'Orders', id: 'orders' },
+                    ];
+                    const onPillClick = (pillId: string) => {
+                      const isPillActive = !activePills.includes(pillId);
+                      const updatedPills = isPillActive
+                        ? [...activePills, pillId]
+                        : activePills.filter((activePillId) => activePillId !== pillId);
 
-              setActivePills(updatedPills);
-            };
-            const cards = [
-              {
-                name: 'Shipping App Pro',
-                description: 'All your shipping needs in a one stop shop.',
-                type: 'shipping',
-              },
-              {
-                name: 'Order Tracker Deluxe',
-                description: 'Track your orders across all your devices.',
-                type: 'orders',
-              },
-              {
-                name: 'Expedited Shipper',
-                description: 'The best rush rates in the country.',
-                type: 'shipping',
-              },
-              {
-                name: 'Inventory Wizard',
-                description: 'Inventory tracking app to cover all your needs.',
-                type: 'other',
-              },
-            ];
-            const isFiltered = Boolean(activePills.length);
-            const filteredCards = cards.filter((card) => activePills.includes(card.type));
-            const appCards = isFiltered ? filteredCards : cards;
+                      setActivePills(updatedPills);
+                    };
+                    const cards = [
+                      {
+                        name: 'Shipping App Pro',
+                        description: 'All your shipping needs in a one stop shop.',
+                        type: 'shipping',
+                      },
+                      {
+                        name: 'Order Tracker Deluxe',
+                        description: 'Track your orders across all your devices.',
+                        type: 'orders',
+                      },
+                      {
+                        name: 'Expedited Shipper',
+                        description: 'The best rush rates in the country.',
+                        type: 'shipping',
+                      },
+                      {
+                        name: 'Inventory Wizard',
+                        description: 'Inventory tracking app to cover all your needs.',
+                        type: 'other',
+                      },
+                    ];
+                    const isFiltered = Boolean(activePills.length);
+                    const filteredCards = cards.filter((card) => activePills.includes(card.type));
+                    const appCards = isFiltered ? filteredCards : cards;
 
-            return (
-              <>
-                <PillTabs activePills={activePills} items={items} onPillClick={onPillClick} />
-                <Flex>
-                  {appCards.map(({ name, description }) => (
-                    <Card description={description} key={name} name={name} />
-                  ))}
-                </Flex>
-              </>
-            );
-          }}
-          {/* jsx-to-string:end */}
-        </CodePreview>
+                    return (
+                      <>
+                        <PillTabs
+                          activePills={activePills}
+                          items={items}
+                          onPillClick={onPillClick}
+                        />
+                        <Flex>
+                          {appCards.map(({ name, description }) => (
+                            <Card description={description} key={name} name={name} />
+                          ))}
+                        </Flex>
+                      </>
+                    );
+                  }}
+                  {/* jsx-to-string:end */}
+                </CodePreview>
+              ),
+            },
+          ]}
+        />
       </Panel>
 
       <Panel header="Props" headerId="props">
