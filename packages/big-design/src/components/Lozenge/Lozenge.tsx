64670ba4d9fdbe56d2e7f659d4ed3d80767ad68f
David Linke <davelinke@users.noreply.github.com>
2025-08-19 15:14:51 -0400

    feat(component): add base changes for new lozenge features

--- packages/big-design/src/components/Lozenge/Lozenge.tsx
+++ packages/big-design/src/components/Lozenge/Lozenge.tsx
@@ -1,31 +1,93 @@
-import { InfoIcon } from '@bigcommerce/big-design-icons';
+import {
+  InfoIcon,
+  // BugReportIcon,
+  // DoNotDisturbOnTotalSilenceIcon,
+  // EventBusyIcon,
+  // RocketLaunchIcon,
+  // ScienceIcon,
+} from '@bigcommerce/big-design-icons';
 import React from 'react';
 
 import { Tooltip } from '../Tooltip';
 
 import { StyledLozenge } from './styled';
 
-export interface LozengeProps {
+interface SharedLozengeProps {
   label: string;
-  tooltipContent?: string;
   variant?: 'alpha' | 'beta' | 'deprecated' | 'legacy' | 'new';
 }
+export interface LozengeWithTooltipProps extends SharedLozengeProps {
+  tooltipContent?: string;
+}
 
-export const Lozenge: React.FC<LozengeProps> = ({ label, tooltipContent, variant }) => {
-  const LozengeElement = (
-    <StyledLozenge hasTooltip={!!tooltipContent} variant={variant}>
+export interface LozengeWithPopoverProps extends SharedLozengeProps {
+  onClick: () => void;
+  isOpen: boolean;
+}
+
+export type LozengeProps = LozengeWithTooltipProps | LozengeWithPopoverProps;
+
+function isPopoverProps(props: LozengeProps): props is LozengeWithPopoverProps {
+  return (
+    typeof (props as LozengeWithPopoverProps).onClick === 'function' &&
+    typeof (props as LozengeWithPopoverProps).isOpen === 'boolean'
+  );
+}
+
+function isTooltipProps(props: LozengeProps): props is LozengeWithTooltipProps {
+  return (
+    'tooltipContent' in props && typeof props.tooltipContent === 'string' && !!props.tooltipContent
+  );
+}
+
+// const iconMapping = {
+//   alpha: <BugReportIcon aria-hidden="true" size="large" />,
+//   beta: <ScienceIcon aria-hidden="true" size="large" />,
+//   deprecated: <DoNotDisturbOnTotalSilenceIcon aria-hidden="true" size="large" />,
+//   legacy: <EventBusyIcon aria-hidden="true" size="large" />,
+//   new: <RocketLaunchIcon aria-hidden="true" size="large" />,
+// };
+
+export const Lozenge = React.forwardRef<HTMLDivElement, LozengeProps>((props, ref) => {
+  const { label, variant } = props;
+
+  if (isPopoverProps(props)) {
+    return (
+      <StyledLozenge
+        ref={ref}
+        variant={variant}
+        role="button"
+        tabIndex={0}
+        onClick={props.onClick}
+        aria-expanded={props.isOpen}
+      >
+        {/* {variant && iconMapping[variant]} */}
+        {label}
+      </StyledLozenge>
+    );
+  }
+
+  if (isTooltipProps(props)) {
+    return (
+      <Tooltip
+        placement="auto"
+        trigger={
+          <StyledLozenge ref={ref} variant={variant} hasTooltip>
+            {label}
+            <InfoIcon aria-hidden="true" size="large" />
+          </StyledLozenge>
+        }
+      >
+        {props.tooltipContent}
+      </Tooltip>
+    );
+  }
+
+  return (
+    <StyledLozenge ref={ref} variant={variant}>
       {label}
-      {tooltipContent ? <InfoIcon aria-hidden="true" size="large" /> : null}
     </StyledLozenge>
   );
-
-  return tooltipContent ? (
-    <Tooltip placement="auto" trigger={LozengeElement}>
-      {tooltipContent}
-    </Tooltip>
-  ) : (
-    LozengeElement
-  );
-};
+});
 
 Lozenge.displayName = 'Lozenge';
