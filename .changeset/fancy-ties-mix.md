a1306f69ed711f30909e629d907b6c1febe7887f
David Linke <davelinke@users.noreply.github.com>
2025-08-19 15:35:54 -0400

    feat(icons): add icons for lozenge (#1725)

---
+++ .changeset/fancy-ties-mix.md
@@ -0,0 +1,5 @@
+---
+'@bigcommerce/big-design-icons': minor
+---
+
+added icons for lozenges
---
+++ packages/big-design-icons/src/components/ScienceIcon.tsx
@@ -0,0 +1,37 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo, useId } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useId();
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+  const ariaHidden = titleId ? undefined : true;
+
+  return (
+    <svg
+      aria-hidden={ariaHidden}
+      aria-labelledby={titleId}
+      fill="currentColor"
+      height={24}
+      ref={svgRef}
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      width={24}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M20.54 17.73 15 11V5h1c.55 0 1-.45 1-1s-.45-1-1-1H8c-.55 0-1 .45-1 1s.45 1 1 1h1v6l-5.54 6.73c-.32.39-.46.83-.46 1.27.01 1.03.82 2 2 2h14c1.19 0 2-.97 2-2 0-.44-.14-.88-.46-1.27" />
+    </svg>
+  );
+};
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const ScienceIcon = memo(createStyledIcon(IconWithForwardedRef));
+ScienceIcon.displayName = 'ScienceIcon';
