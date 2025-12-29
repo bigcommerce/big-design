da04ddfdd73d064e77de755c749f8c4a00636007
David Linke <davelinke@users.noreply.github.com>
2025-12-29 10:11:02 -0500

    feat(icons): add base chatbot icons

---
+++ packages/big-design-icons/src/components/ThumbUpOffAltIcon.tsx
@@ -0,0 +1,37 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo, useId } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useId();
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+  const ariaHidden = titleId ? undefined : true;
+
+  return (
+    <svg
+      aria-hidden={ariaHidden}
+      aria-labelledby={titleId}
+      fill="currentColor"
+      height={24}
+      ref={svgRef}
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      width={24}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M13.12 2.06 7.58 7.6c-.37.37-.58.88-.58 1.41V19c0 1.1.9 2 2 2h9c.8 0 1.52-.48 1.84-1.21l3.26-7.61C23.94 10.2 22.49 8 20.34 8h-5.65l.95-4.58c.1-.5-.05-1.01-.41-1.37-.59-.58-1.53-.58-2.11.01M3 21c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2s-2 .9-2 2v8c0 1.1.9 2 2 2" />
+    </svg>
+  );
+};
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const ThumbUpOffAltIcon = memo(createStyledIcon(IconWithForwardedRef));
+ThumbUpOffAltIcon.displayName = 'ThumbUpOffAltIcon';
