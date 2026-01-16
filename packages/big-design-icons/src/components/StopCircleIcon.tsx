9d7a9258b441d2d855a629ca95bf8ab9af511995
David Linke <davelinke@users.noreply.github.com>
2026-01-16 09:47:06 -0500

    feat(icons): add play and stop chat input icons

---
+++ packages/big-design-icons/src/components/StopCircleIcon.tsx
@@ -0,0 +1,37 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo, useId } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useId();
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+  const ariaHidden = titleId ? undefined : true;
+
+  return (
+    <svg
+      aria-hidden={ariaHidden}
+      aria-labelledby={titleId}
+      fill="currentColor"
+      height={24}
+      ref={svgRef}
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      width={24}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m3 14H9c-.55 0-1-.45-1-1V9c0-.55.45-1 1-1h6c.55 0 1 .45 1 1v6c0 .55-.45 1-1 1" />
+    </svg>
+  );
+};
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const StopCircleIcon = memo(createStyledIcon(IconWithForwardedRef));
+StopCircleIcon.displayName = 'StopCircleIcon';
