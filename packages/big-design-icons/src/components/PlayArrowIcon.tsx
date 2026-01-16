9a7e55883d991dd5d6611b27047bb7946d2958d6
David Linke <davelinke@users.noreply.github.com>
2026-01-16 10:45:34 -0500

    feat(icons): add play and stop chat input icons (#1819)
    * feat(icons): add play and stop chat input icons
    
    * feat(icons): add changeset

---
+++ packages/big-design-icons/src/components/PlayArrowIcon.tsx
@@ -0,0 +1,36 @@
+// **********************************
+// Auto-generated file, do NOT modify
+// **********************************
+import React, { forwardRef, memo, useId } from 'react';
+
+import { createStyledIcon, IconProps, PrivateIconProps } from '../base';
+
+const Icon: React.FC<IconProps & PrivateIconProps> = ({ svgRef, title, theme, ...props }) => {
+  const uniqueTitleId = useId();
+  const titleId = title ? props.titleId || uniqueTitleId : undefined;
+  const ariaHidden = titleId ? undefined : true;
+
+  return (
+    <svg
+      aria-hidden={ariaHidden}
+      aria-labelledby={titleId}
+      fill="currentColor"
+      height={24}
+      ref={svgRef}
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      width={24}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18a1 1 0 0 0 0-1.69L9.54 5.98A.998.998 0 0 0 8 6.82" />
+    </svg>
+  );
+};
+const IconWithForwardedRef = forwardRef<SVGSVGElement, IconProps>((iconProps, ref) => (
+  <Icon {...iconProps} svgRef={ref} />
+));
+
+export const PlayArrowIcon = memo(createStyledIcon(IconWithForwardedRef));
+PlayArrowIcon.displayName = 'PlayArrowIcon';
