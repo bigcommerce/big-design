869c7f09a09d3a637f8a3383ff058f0078f2cf97
Chancellor Clark <chancellor.clark@bigcommerce.com>
2026-05-12 11:57:09 -0600

    chore(docs): remove deprecated UX writing guide and eslint-plugin-mdx
    The design-docs/ux-writing content was added to support the BigCommerce
    developer-center repo but is no longer needed in this repository.
    Removing it also allows us to remove the eslint-plugin-mdx dependency
    and its MDX-specific ESLint configuration.

--- packages/docs/.eslintrc.js
+++ packages/docs/.eslintrc.js
@@ -3,19 +3,4 @@ module.exports = {
   root: true,
   extends: [require.resolve('@bigcommerce/configs/eslint/base.js')],
   ignorePatterns: ['.next', 'out', 'node_modules', 'next-env.d.ts'],
-  overrides: [
-    {
-      files: ['design-docs/**/*.mdx'],
-      extends: ['plugin:mdx/recommended', 'plugin:react/recommended'],
-      settings: {
-        react: {
-          version: 'detect',
-        },
-      },
-      rules: {
-        // These ignored files are resolved when they are consumed by the BigCommerce developer-center repo.
-        'import/no-unresolved': ['error', { ignore: ['^@components/.+'] }],
-      },
-    },
-  ],
 };
